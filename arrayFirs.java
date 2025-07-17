34f01a795a8f6c4fec29b052720d2f44fa32f0be
prince711 <princemeena7111@gmail.com>
2025-07-17 11:43:47 -0700

    Data Structure & Algo. start || arrays Ch.1 || HELLO WORLD ||

---
+++ arrayFirs.java
@@ -0,0 +1,7 @@
+package dsajava;
+public class arrayFirs{
+    public static void main(String args[]){
+        System.out.println("hello world {1}");
+        System.out.println("|| Java || DSA Ch.1 || Arrays || 18 jul 2025 :00:10");
+    }
+}
