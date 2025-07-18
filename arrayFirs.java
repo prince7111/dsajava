da4bedcec77faff0df8a03542a800db4f41e9cd3
prince711 <princemeena7111@gmail.com>
2025-07-18 09:13:57 -0700

    Q.5 || subArray print

--- arrayFirs.java
+++ arrayFirs.java
@@ -49,11 +49,26 @@ public class arrayFirs{
        }
 
         }
-    public static void main(String args[]){
+        public static void subArray(int array[]){
+             int n = array.length-1;
+       for(int i =0;i<n ;i++){
+        for(int j =i;j<=n;j++){
+          
+
+       
+        for(int k=i;k<=j;k++){
+            System.out.print(array[k]+" ");
+        }
+        System.out.println();
+        }
+       }
+     
+
+        }
+    public static void main(String args[]){                 
        int array [] = {2,4,6,8,10};
-       printPairs(array);
-     
-     
+       //Q.5 print all subarray
+     subArray(array);
         }
         
     }
