3f372494d884ac23894d53c56fef1d427a90a913
prince711 <princemeena7111@gmail.com>
2025-07-18 20:10:54 -0700

    subArrays sum code compleate \Successfully\

--- arrayTwo.java
+++ arrayTwo.java
@@ -1,7 +1,23 @@
 package dsajava;
+
 public class arrayTwo{
+    //subarray
+    public static void subArray(int array[]){
+        
+        for(int i =0; i< array.length;i++){
+            for(int j=i; j<array.length;j++){
+                for(int k =i;k<=j; k++){
+                    System.out.print(array[k]+" ");
+                }
+                System.out.println(); 
+            }
+        }
+    }
     public static void main(String args[]){
-        System.out.println("Hello world (I)");
-        System.out.println("|| JAVA || dsa Ch.1 || Arrays, part II ||");
+       // System.out.println("Hello world (I)");
+       //System.out.println("|| JAVA || dsa Ch.1 || Arrays, part II ||");
+        //Q.1 print and sum of subarray
+        int array [] = {2,4,6,8,10};
+        subArray(array);
     }
 }
