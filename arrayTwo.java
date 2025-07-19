667bd211a4f7aaa9ff97e4ee5d3d8a2083594041
prince711 <princemeena7111@gmail.com>
2025-07-18 20:18:00 -0700

    princt sub array and their sum \

--- arrayTwo.java
+++ arrayTwo.java
@@ -13,11 +13,26 @@ public class arrayTwo{
             }
         }
     }
+    public static void subArrSum(int array[]){
+            for(int i =0; i< array.length;i++){
+            for(int j=i; j<array.length;j++){
+                int currSum =0;
+                for(int k =i;k<=j; k++){
+                    System.out.print(array[k]+" ");
+                    currSum+= array[k];
+                } System.out.println();
+                System.out.println("current Sum ="+currSum);
+                
+            }
+        }
+
+    }
     public static void main(String args[]){
        // System.out.println("Hello world (I)");
        //System.out.println("|| JAVA || dsa Ch.1 || Arrays, part II ||");
         //Q.1 print and sum of subarray
         int array [] = {2,4,6,8,10};
-        subArray(array);
+        // subArray(array);
+        subArrSum(array);
     }
 }
