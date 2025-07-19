f74fccd09e9458d349c1c519a693a8650bef1998
prince711 <princemeena7111@gmail.com>
2025-07-18 20:29:20 -0700

    Q.2|| MAX SUB ARRAY SUM ||

--- arrayTwo.java
+++ arrayTwo.java
@@ -2,37 +2,59 @@ package dsajava;
 
 public class arrayTwo{
     //subarray
-    public static void subArray(int array[]){
+    // public static void subArray(int array[]){
         
-        for(int i =0; i< array.length;i++){
-            for(int j=i; j<array.length;j++){
-                for(int k =i;k<=j; k++){
-                    System.out.print(array[k]+" ");
-                }
-                System.out.println(); 
-            }
-        }
-    }
-    public static void subArrSum(int array[]){
-            for(int i =0; i< array.length;i++){
+    //     for(int i =0; i< array.length;i++){
+    //         for(int j=i; j<array.length;j++){
+    //             for(int k =i;k<=j; k++){
+    //                 System.out.print(array[k]+" ");
+    //             }
+    //             System.out.println(); 
+    //         }
+    //     }
+    // }
+    //Q.1 subarray sum
+    // public static void subArrSum(int array[]){
+    //         for(int i =0; i< array.length;i++){
+    //         for(int j=i; j<array.length;j++){
+    //             int currSum =0;
+    //             for(int k =i;k<=j; k++){
+    //                 System.out.print(array[k]+" ");
+    //                 currSum+= array[k];
+    //             } System.out.println();
+    //             System.out.println("current Sum ="+currSum);
+                
+    //         }
+    //     }
+
+    // }
+    
+    //Q.2 MAX SUBARRAY SUM
+    public static void maxSubArraySum(int array[]){
+        int maxSum =Integer.MIN_VALUE;
+                    for(int i =0; i< array.length;i++){
             for(int j=i; j<array.length;j++){
                 int currSum =0;
                 for(int k =i;k<=j; k++){
                     System.out.print(array[k]+" ");
                     currSum+= array[k];
-                } System.out.println();
-                System.out.println("current Sum ="+currSum);
-                
-            }
-        }
+                } 
+                System.out.println();
+                System.out.println("curent sum ="+currSum);
+                if(currSum > maxSum){
+                    maxSum = currSum;
 
+                }                
+            }            
+        }
+        System.out.println("your maxSubArray sum is ="+maxSum);
     }
+    
     public static void main(String args[]){
        // System.out.println("Hello world (I)");
        //System.out.println("|| JAVA || dsa Ch.1 || Arrays, part II ||");
-        //Q.1 print and sum of subarray
-        int array [] = {2,4,6,8,10};
-        // subArray(array);
-        subArrSum(array);
+       int array[] = {2,4,6,8,10};
+       maxSubArraySum(array);
+        
     }
 }
