0392280efacc3dbe2da0c37eb7a768b3d972c0b6
prince711 <princemeena7111@gmail.com>
2025-07-21 08:04:59 -0700

    nahi hua

--- dsaQ.java
+++ dsaQ.java
@@ -6,23 +6,17 @@ public class dsaQ {
     public static void main(String args[]){
         System.out.println("Hello wolrld");
         System.out.println("dsa array || Q.2");
-        int grid [][] = {{1,2},{4,5}};
-        int totalNum =grid.length;
-        int excNUm = grid.length+1;
-        int totalSum = (excNUm*(excNUm+1))/2;
-        int sum =0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-                sum += grid[i][j];
+        int grid[] = {1,2,2,5};
+        for(int i =0;i<grid.length;i++){
+            for(int j =0;j<grid.length;j++){
+                if(grid[i]==grid[j] && i != j){
+                    System.out.println(grid[j]+" ");
+                }
             }
+
+                        
         }
-       System.out.println(totalSum+" "+ sum);
-        int missing = totalSum - sum;
-        System.out.println("missing number is"+missing);
-
-
-
-
+     
     }
 
     
