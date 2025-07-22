84ca6342a528bbd2a989c55fb2976f2d5c2f4274
prince711 <princemeena7111@gmail.com>
2025-07-22 05:51:21 -0700

    dsaQ.3 ||array easy 3 || Merge Two Sorted Array ,atrixes||

--- dsaQ.java
+++ dsaQ.java
@@ -3,21 +3,50 @@ package dsajava;
 
 
 public class dsaQ {
-    public static void main(String args[]){
-        System.out.println("Hello wolrld");
-        System.out.println("dsa array || Q.2");
-        int grid[] = {1,2,2,5};
-        for(int i =0;i<grid.length;i++){
-            for(int j =0;j<grid.length;j++){
-                if(grid[i]==grid[j] && i != j){
-                    System.out.println(grid[j]+" ");
-                }
+   public static void mergeTwo(int A[],int B[],int m,int n){
+    int idx = (m+n)-1;
+        int i =m-1;
+        int j=n-1;
+        while(i>=0 && j>=0){
+            if(A[i]>B[j]){
+                A[idx] = A[i];
+                idx--; i--;
+            }else{
+                A[idx] = B[j];
+            idx--;j--;
             }
-
-                        
         }
-     
+        while(j>=0){
+            A[idx] = B[j];
+                idx--;j--;
+
+        }
+         for(int k =0;k<A.length;k++){
+        System.out.print(A[k]+" ");
+       }
+       System.out.println();
+
     }
+   
+    public static void main(String args[]){
+        int A[] ={1,2,3,0,0,0};
+        int m =3;
+        int B[] = {2,5,6};
+        int n =3;
+        mergeTwo(A, B, m, n);
+      
+    }
+    
+
+        //Q.3 merge 2 sorted array
+ 
+}
+
+
+      
+                        
+        
+     
+     
 
     
-}
