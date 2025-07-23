78663205df15db415557be045a1377b64236a997
prince711 <princemeena7111@gmail.com>
2025-07-23 09:18:45 -0700

    array || Q.4 || single numner ||

--- dsaQs.java
+++ dsaQs.java
@@ -3,6 +3,13 @@ public class dsaQs {
     public static void main(String args[]){
         System.out.println("hello world");
         System.out.println("DSA JAVA || ARRAY 1 || EASY LEVEL || Q.4 || Single number");
+        int array[] = {4,2,1,2,1};
+        int single =0;
+        for(int i =0; i<array.length; i++){
+            single = single^array[i];
+
+        }
+        System.out.println("single :"+single);
     }
     
 }
