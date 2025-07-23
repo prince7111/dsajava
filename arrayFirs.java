6b0a81c19b03a5c77b7ea10857593a87f57692d9
prince711 <princemeena7111@gmail.com>
2025-07-23 06:07:46 -0700

    ||Array part II || Q.4 || Trapeing rain water ||

--- arrayFirs.java
+++ arrayFirs.java
@@ -1,74 +1,84 @@
 package dsajava;
 
 public class arrayFirs{
-    public static int linearSearch(int array[],int key){
-        for(int i =0; i<array.length; i++){
-            if(array[i] == key){
-                return i;
-            }
-        }
-        return -1;
+    // public static int linearSearch(int array[],int key){
+    //     for(int i =0; i<array.length; i++){
+    //         if(array[i] == key){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
 
-    }  
+    // }  
      //Q.2 largest Number FIND 
-    public static int largestNumber(int array2 []){
-        int largest =0;
-        for(int i =0;i<array2.length; i++){
-            if(array2[i] > largest){
-                largest = array2[i];
-            }
+    // public static int largestNumber(int array2 []){
+    //     int largest =0;
+    //     for(int i =0;i<array2.length; i++){
+    //         if(array2[i] > largest){
+    //             largest = array2[i];
+    //         }
 
-        }
-        System.out.println("largest number is:"+largest);
-        return 1;
+    //     }
+    //     System.out.println("largest number is:"+largest);
+    //     return 1;
 
-        }
+    //     }
         //Q.3 Binary Search
-        public static int binarySearch(int array3 [], int key){
-            int start =0; int end = array3.length-1;
-            while(start<= end){
-                int mid = (start+end)/2;
-                if(array3[mid]== key){
-                    return mid;
-                }else if(array3[mid] < key){
-                    start = mid+1;
-                }else{
-                    end = mid-1;
-                }
-            }
-            return -1;
-        }
+        // public static int binarySearch(int array3 [], int key){
+        //     int start =0; int end = array3.length-1;
+        //     while(start<= end){
+        //         int mid = (start+end)/2;
+        //         if(array3[mid]== key){
+        //             return mid;
+        //         }else if(array3[mid] < key){
+        //             start = mid+1;
+        //         }else{
+        //             end = mid-1;
+        //         }
+        //     }
+        //     return -1;
+        // }
 
         //print Sub array
-        public static void printPairs(int array[]){
-                   for(int i=0; i<array.length; i++){
-                   for(int j =i+1; j<array.length; j++){
-                   System.out.print("("+array[i]+","+array[j]+")"+",");
-                  }
-                    System.out.println();
-       }
+    //     public static void printPairs(int array[]){
+    //                for(int i=0; i<array.length; i++){
+    //                for(int j =i+1; j<array.length; j++){
+    //                System.out.print("("+array[i]+","+array[j]+")"+",");
+    //               }
+    //                 System.out.println();
+    //    }
 
-        }
-        public static void subArray(int array[]){
-             int n = array.length-1;
-       for(int i =0;i<n ;i++){
-        for(int j =i;j<=n;j++){
+    //     }
+         //Q.5 print all subarray
+    //     public static void subArray(int array[]){
+    //          int n = array.length-1;
+    //    for(int i =0;i<n ;i++){
+    //     for(int j =i;j<=n;j++){
           
 
        
-        for(int k=i;k<=j;k++){
-            System.out.print(array[k]+" ");
-        }
-        System.out.println();
-        }
-       }
+    //     for(int k=i;k<=j;k++){
+    //         System.out.print(array[k]+" ");
+    //     }
+    //     System.out.println();
+    //     }
+    //    }
      
 
-        }
-    public static void main(String args[]){                 
-       int array [] = {2,4,6,8,10};
-       //Q.5 print all subarray
-     subArray(array);
-        }
-        
+    //
+    
+    public static void main(String args[]){       
+        int arr[]   = {2,4,6,8,10};int n = arr.length-1;
+        for(int i=0;i<=n; i++){
+          
+            for(int j=i; j<=n; j++){
+                for(int k=i; k<=j; k++){
+                    
+                    System.out.print(arr[k]+" ");
+                }
+                            System.out.println();
+            }
+
+        }    
     }
+}
--- dsaQ.java
+++ dsaQ.java
@@ -3,37 +3,8 @@ package dsajava;
 
 
 public class dsaQ {
-   public static void mergeTwo(int A[],int B[],int m,int n){
-    int idx = (m+n)-1;
-        int i =m-1;
-        int j=n-1;
-        while(i>=0 && j>=0){
-            if(A[i]>B[j]){
-                A[idx] = A[i];
-                idx--; i--;
-            }else{
-                A[idx] = B[j];
-            idx--;j--;
-            }
-        }
-        while(j>=0){
-            A[idx] = B[j];
-                idx--;j--;
-
-        }
-         for(int k =0;k<A.length;k++){
-        System.out.print(A[k]+" ");
-       }
-       System.out.println();
-
-    }
-   
-    public static void main(String args[]){
-        int A[] ={1,2,3,0,0,0};
-        int m =3;
-        int B[] = {2,5,6};
-        int n =3;
-        mergeTwo(A, B, m, n);
+        public static void main(String args[]){
+        
       
     }
     
