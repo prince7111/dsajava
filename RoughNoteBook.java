dad5fe43430bb13cb301220e327033a09ac3f33e
prince711 <princemeena7111@gmail.com>
2025-07-23 09:14:33 -0700

    Array Q.4 || Compleate successfuly||

--- RoughNoteBook.java
+++ RoughNoteBook.java
@@ -3,6 +3,7 @@ package dsajava;
 public class RoughNoteBook{
   
     public static void main(String[] args) {
+
       
 
     }
---
+++ dsaQs.java
@@ -0,0 +1,8 @@
+package dsajava;
+public class dsaQs {
+    public static void main(String args[]){
+        System.out.println("hello world");
+        System.out.println("DSA JAVA || ARRAY 1 || EASY LEVEL || Q.4 || Single number");
+    }
+    
+}
