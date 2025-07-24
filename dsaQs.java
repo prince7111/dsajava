7be3cfca74db62e6c12cff268be7f0434168d225
prince711 <princemeena7111@gmail.com>
2025-07-24 02:33:21 -0700

    Q.4 || trapered RAin Water ||...||

--- dsaQs.java
+++ dsaQs.java
@@ -34,6 +34,8 @@ public class dsaQs {
         return -1;
 
     }
+                   // Q.3 CALCULATE MAXIMUM PROFIT
+
     public static int calCprofit(int price[]){
         int bysPrice =Integer.MAX_VALUE;
         int maxProfit = 0;
@@ -49,11 +51,38 @@ public class dsaQs {
         return maxProfit;
 
     }
+    //Q.4 Given a non negeatic repesentin map where th width os each bar is ,compute how
+    // much water it can trap after raining
+    public static int calCTrapwater(int height[],int n){
+        int trpWater =0;
+        //calculate left max bounry
+        int leftMax[] = new int [n];
+        leftMax[0] = height[0];
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(leftMax[i-1],height[i]);
+        }
+        //CALCULATE RIGHT MAX BOUNDRY
+        int rightMax[] = new int [n];
+        rightMax[n-1] = height[n-1];
+        for(int i =n-2; i>=0; i--){
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+        }
+        for(int i =0; i<n ; i++){
+            int waterlevel = Math.min(leftMax[i],rightMax[i]);
+            trpWater += waterlevel - height[i];
+        }
+        System.out.println("our output is:");
+        return trpWater;
+
+
+    }
     public static void main(String args[]){
-        int prince[] = {7,1,5,3,6,4};
-        System.out.println( calCprofit(prince));
+        int height[] ={0,1,0,2,1,0,1,3,2,1,2,1};//output 6
+        int n = height.length;
+        System.out.println(calCTrapwater(height, n));
+     
+
         
-       //
         
       
     
