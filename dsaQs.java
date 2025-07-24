770c4a932295b2a2b988d618b0bf41d669852db4
prince711 <princemeena7111@gmail.com>
2025-07-24 02:00:59 -0700

    JAVA DSA || ARRAYS 2 || Q.2 || FIND INDEX OF GIVEN TARGEST || COMPLEATE BRUTE FOERCE Successfully...|

--- dsaQs.java
+++ dsaQs.java
@@ -1,6 +1,11 @@
 package dsajava;
 public class dsaQs {
-    public static boolean  isTwice(int nums[]){
+    //dsa || Array assignment Q.1 || isTwice 
+    public static boolean  isTwice(int nums[]){ 
+        //System.out.println("Array part II || Assignment Q.1 || isTWice retun treue");
+       // System.out.println("java dsa || Assignment || Quesions ||");
+       
+
        boolean res =false;
         
         for(int i =0;i<nums.length;i++){
@@ -17,11 +22,23 @@ public class dsaQs {
         return res;  
         
     }
+      // ARRAYS ASSIGNMENT Q. 2
+      //july 2025
+    public static int findIdx(int nums[],int target){ 
+        for(int i=0;i<nums.length; i++){
+            if(nums[i]==target){
+                System.out.println("our target"+target+"'s index is ="+i);
+                return i;
+            }
+        }
+        return -1;
+
+    }
     public static void main(String args[]){
-       int nums[] = {1,1,3,3,4,3,2,4,2};
-       System.out.println(isTwice(nums));
-
-
+       int nums[] = {4,5,6,7,0,1,2};
+        int target = 4;
+        System.out.println(findIdx(nums, target));
+        
       
     
     }
