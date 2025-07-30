15281ce9cb4fa00060d74bab157b030290e8b027
prince711 <princemeena7111@gmail.com>
2025-07-30 10:04:12 -0700

    sortcolour || Q.10

--- dsaQs.java
+++ dsaQs.java
@@ -1,5 +1,4 @@
 package dsajava;
-
 public class dsaQs {
     //dsa || Array assignment Q.1 || isTwice 
     // public static boolean  isTwice(int nums[]){ 
@@ -170,15 +169,25 @@ public class dsaQs {
 //        return maxWater;   
 //     }
 
-    
-    public static void main(String args[]){
-        System.out.println("hello World");//
-        System.out.println("ArrayS Q.10||Sort array 0s ,1s ,2s");//30 july
-        
-      
-      
-       
-      
+    public static void sortcolour(int nums[]){
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=0; j<nums.length-1-i; j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        for(int i =0; i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
 
     }
+     
+    public static void main(String args[]){
+        int nums[] = {2,0,2,1,1,0};
+        sortcolour(nums);
+    }
 }
+
