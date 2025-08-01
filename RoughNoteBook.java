eb2976bb6deedd55202b72bc72749a77382c435e
prince711 <princemeena7111@gmail.com>
2025-08-01 11:09:02 -0700

    Dsa || Arrays III || d3 Q.13 || Next Permutation ||

--- RoughNoteBook.java
+++ RoughNoteBook.java
@@ -1,27 +1,17 @@
 package dsajava;
-import java.util.ArrayList;
 
 public class RoughNoteBook{
     public static void main(String args[]){ 
-     int nums[] ={-1,0,1,2,-1,-4};
-   
-     for(int i =0;i<nums.length;i++){
-      for(int j =i+1; j<nums.length; j++){
-        for(int k =j+1; k<nums.length; k++){
-          if((nums[i]+nums[j]+nums[k]==0)&& i!=j && i!=k && j!=k){
-            ArrayList<Integer> list1 = new ArrayList<>();
-            list1.add(nums[i]);
-            list1.add(nums[j]);
-            list1.add(nums[k]);
-            System.out.println(list1);
+      int nums[] = {1,2,3};
+   for(int i =0; i<nums.length;i++){
+    for(int j=i+1; j<nums.length; j++){
+      for(int k =j+1;k<nums.length;k++){
+        
 
-
-          }
-        }
       }
-
-     }
-     
+    }
+   }
+  
 
 
     }
--- arrayFirs.java
+++ arrayFirs.java
@@ -66,19 +66,28 @@ public class arrayFirs{
      
 
     //
-    
-    public static void main(String args[]){       
-        int arr[]   = {2,4,6,8,10};int n = arr.length-1;
-        for(int i=0;i<=n; i++){
-          
-            for(int j=i; j<=n; j++){
-                for(int k=i; k<=j; k++){
-                    
-                    System.out.print(arr[k]+" ");
-                }
-                            System.out.println();
-            }
-
-        }    
+    public static void merge(int []nums1,int m,int []nums2,int n){
+      int i =m-1; int j =n-1;int idx =m+n-1;
+      while(i>=0 && j>=0){
+        if(nums1[i]>=nums2[j]){
+          nums1[idx--] = nums1[i--];
+        }else{
+          nums1[idx--] = nums2[j--];
+                  }
+          while(j>=0){
+             nums1[idx--] = nums2[j--];
+        }  
+      }
+    }
+    public static void main(String args[]){ 
+      int nums1[] = {1,2,3,0,0,0};int m =3;
+      int nums2[] ={2,5,6};int n =3;
+      merge(nums1, m, nums2, n);
+      for(int i =0;i<nums1.length;i++){
+        System.out.println(nums1[i]+" ");
+      }
+      System.out.println();
+        
+        
     }
 }
