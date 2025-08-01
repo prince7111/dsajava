bb55f44598c579fe6bf197218d6df36a51b33850
prince711 <princemeena7111@gmail.com>
2025-08-01 12:22:53 -0700

    copey paste || next permutation||

--- dsaQs.java
+++ dsaQs.java
@@ -219,12 +219,82 @@ public class dsaQs {
 //         }
         
 //     }
+//   public static  void nextPermutation(int[] nums) {
+//          int i = nums.length - 1;
+//         while (i > 0 && nums[i-1] >= nums[i]) {
+//             i--;
+//         }
+//         if (i == 0) {
+//             reverse(nums, 0, nums.length-1);
+//             return;
+//         }
+//         int j = nums.length - 1;
+//         while (j >= i && nums[j] <= nums[i-1]) {
+//             j--;
+//         }
+//         swap(nums, i-1, j);
+//         reverse(nums, i, nums.length-1);
+//     }
 
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        System.out.println("|java dsa | d2 Q 13 || ............/,,,,,///,,..../");
-        System.err.println("1");
-      
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+
+//     private void reverse(int[] nums, int start, int end) {
+//         while (start < end) {
+//             int temp = nums[start];
+//             nums[start] = nums[end];
+//             nums[end] = temp;
+//             start++;
+//             end--;
+//         }
+        
+//     }
+
+
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1);
+    }
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        
+
+        nextPermutation(nums);
+        
+        // आउटपुट: [1, 3, 2]
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
     }
 }
 
