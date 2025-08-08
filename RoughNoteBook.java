aab9ca6f4288c06c468927c2857e1428dbfdfc0c
prince711 <princemeena7111@gmail.com>
2025-08-08 08:00:29 -0700

    selection sort

--- RoughNoteBook.java
+++ RoughNoteBook.java
@@ -1,23 +1,41 @@
 package dsajava;
 
 public class RoughNoteBook{
-  public static void bubbleSort(int arr[]){
+//   public static void bubbleSort(int arr[]){
+//     for(int i =0; i<arr.length-1;i++){
+      
+//         for(int j =0;j<arr.length-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+// }
+  public static void selectionSort(int arr[]){
     for(int i =0; i<arr.length-1;i++){
-        for(int j =0;j<arr.length-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+      
+      int minPos =i;
+        for(int j =i+1;j<arr.length;j++){
+          if(arr[minPos]> arr[j]){
+            minPos =j;
+          }
             }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
         }
     }
-}
+
 
     public static void main(String args[]){ 
       int arr[] = {5,4,1,3,2};
-      bubbleSort(arr);
+      selectionSort(arr);
+      
       for(int i =0; i<arr.length;i++){
         System.out.print(arr[i]+" ");
       }
     }
   }
+  
