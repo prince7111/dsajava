fa9f5999bf8b68fdf938c76996e2d9cdde308950
prince711 <princemeena7111@gmail.com>
2025-08-09 05:01:09 -0700

    Insertion sort

--- RoughNoteBook.java
+++ RoughNoteBook.java
@@ -13,29 +13,40 @@ public class RoughNoteBook{
 //         }
 //     }
 // }
-  public static void selectionSort(int arr[]){
-    for(int i =0; i<arr.length-1;i++){
+  // public static void selectionSort(int arr[]){
+  //   for(int i =0; i<arr.length-1;i++){
       
-      int minPos =i;
-        for(int j =i+1;j<arr.length;j++){
-          if(arr[minPos]> arr[j]){
-            minPos =j;
-          }
-            }
-            int temp = arr[minPos];
-            arr[minPos] = arr[i];
-            arr[i] = temp;
+  //     int minPos =i;
+  //       for(int j =i+1;j<arr.length;j++){
+  //         if(arr[minPos]> arr[j]){
+  //           minPos =j;
+  //         }
+  //           }
+  //           int temp = arr[minPos];
+  //           arr[minPos] = arr[i];
+  //           arr[i] = temp;
+  //       }
+  //   }
+
+
+    public static void insertionSort(int arr[]){
+      for(int i =1;i<arr.length;i++){
+        int current = arr[i];
+        int previos = i-1;
+        while(previos>=0 && arr[previos]> current){
+          arr[previos+1] = arr[previos];
+          previos--;
         }
+        arr[previos+1] = current;
+      }
     }
-
-
     public static void main(String args[]){ 
       int arr[] = {5,4,1,3,2};
-      selectionSort(arr);
-      
+      insertionSort(arr);
       for(int i =0; i<arr.length;i++){
         System.out.print(arr[i]+" ");
       }
+   
     }
   }
   
