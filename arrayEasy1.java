9f7d398f6bdc69d671a0ac3282b666f6740912c0
prince711 <princemeena7111@gmail.com>
2025-08-12 11:21:50 -0700

    buy and sell stocks

--- arrayEasy1.java
+++ arrayEasy1.java
@@ -64,9 +64,27 @@ public class arrayEasy1 {
             return res;
 
         }
+        public static int buyAndSellStocks(int price[]){
+            int buyPrice = Integer.MAX_VALUE;
+            int maxProfit = 0;
+            for(int i =0; i<price.length;i++){
+                if(buyPrice < price[i]){
+                    int profit = price[i]-buyPrice;
+                    maxProfit = Math.max(profit, maxProfit);
+                }else{
+                    buyPrice = price[i];
+                }
+
+            }
+            return maxProfit;
+        }
     public static void main(String args[]){
-        int nums[] = {2,1,2}; 
-        System.out.println(singleNumberOP(nums));
+        // Input: prices = [7,1,5,3,6,4]
+        int price[] ={7,1,5,3,6,4};
+        System.out.println(buyAndSellStocks(price));
+       
+
+
       
         
    
