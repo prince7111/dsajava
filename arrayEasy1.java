8a69cf38cf2795926f9ad645d952056c790c2fa3
prince711 <princemeena7111@gmail.com>
2025-08-12 07:11:11 -0700

    majority elemnt || optimize|| arrays 1 || Q1 ||

--- arrayEasy1.java
+++ arrayEasy1.java
@@ -1,21 +1,33 @@
 package dsajava;
-import java.util.*;
-
 public class arrayEasy1 {
-    public static int majorityElement(int nums[]){
-        int maj = nums.length/2;
-        Arrays.sort(nums);
-        return nums[maj];
-    
+    public static int majorityElementOp(int nums[]){ // optimized
+        int maj = nums[0];
+        int count = 1;
+        for(int i =1; i<nums.length; i++){
+            if(nums[i]==maj){
+                count++;
+            }else if(count<=0){
+                maj=nums[i];count=1;
+            }else{
+                count--;
+            }
+            
 
+        }
+        return maj;
 
     }
+    // public static int majorityElement(int nums[]){ // brute force
+    //     int maj = nums.length/2;
+    //     Arrays.sort(nums);
+    //     return nums[maj];
+    //     }
     public static void main(String args[]){
-    System.out.println("hello world");
-    System.out.println("|| Java dsa|| Arrays 1 || Questions 1 to 5  ||");
+     System.out.println("|| Java dsa || Arrays 1 || Questions 1 to 5  ||");
    
     int nums[] = {2,2,1,1,1,2,2};
-    System.out.println(majorityElement(nums));
+    System.out.println(majorityElementOp(nums));
+   // System.out.println(majorityElement(nums));
    
     
 
