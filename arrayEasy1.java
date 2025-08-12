a70cca5dbd90e02c139e6bfd4fc3fe13e8aef8c9
prince711 <princemeena7111@gmail.com>
2025-08-12 06:26:12 -0700

    Java Array 1 || Q .1 ||

---
+++ arrayEasy1.java
@@ -0,0 +1,8 @@
+package dsajava;
+public class arrayEasy1 {
+    public static void main(String args[]){
+    System.out.println("hello world");
+    System.out.println("Java dsa|| Arrays D1 || Questions 1 to 5  ||");
+    }
+    
+}
