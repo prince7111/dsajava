3f8eba8d86c4bd01af163e18b7b214816273221f
prince711 <princemeena7111@gmail.com>
2025-08-12 10:48:09 -0700

    singlr number || brute force ||

--- arrayEasy1.java
+++ arrayEasy1.java
@@ -1,6 +1,6 @@
 package dsajava;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class arrayEasy1 {
     // public static int majorityElementOp(int nums[]){ // optimized
@@ -22,34 +22,49 @@ public class arrayEasy1 {
     //     Arrays.sort(nums);
     //     return nums[maj];
     //     }
-    public static void merge(int nums1[],int m,int nums2[],int n){
-        int i =m-1;
-        int j = n-1;
-        int idx = (m+n)-1;
-        while(i>=0 & j>=0){
-            if(nums2[j]>nums1[i]){
-                nums1[idx--] = nums2[j--];
+    // public static void merge(int nums1[],int m,int nums2[],int n){
+    //     int i =m-1;
+    //     int j = n-1;
+    //     int idx = (m+n)-1;
+    //     while(i>=0 & j>=0){
+    //         if(nums2[j]>nums1[i]){
+    //             nums1[idx--] = nums2[j--];
                 
-            }else{
-                nums1[idx--]= nums1[i--];
+    //         }else{
+    //             nums1[idx--]= nums1[i--];
+    //         }
+    //     }
+    //     while(j>=0){
+    //          nums1[idx--] = nums2[j--];
+
+    //     }
+      
+    // }
+    public static int singleNUm(int nums[]){//brute force 
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+            
+            }
+             for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
             }
         }
-        while(j>=0){
-             nums1[idx--] = nums2[j--];
+        return -1;
+            
+
 
         }
-      
-    }
+        
+
+        
+    
+   
     public static void main(String args[]){
-    
-     
-     int nums1[] ={1,2,3,0,0,0};
-     int m =3;
-     int nums2[] ={2,5,6};
-     int n =3;
-     merge(nums1, m, nums2, n);
-    
-       System.out.println(Arrays.toString(nums1));
-       }
-    
+        int nums[] = {2,2,1}; ///1
+        System.out.println(singleNUm(nums));
+        
+   
+    }    
 }
