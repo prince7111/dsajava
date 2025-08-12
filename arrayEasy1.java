e70a7bcab1f8893828bf360098ca3fff8cb7c37d
prince711 <princemeena7111@gmail.com>
2025-08-12 07:42:42 -0700

    merge tow sorted array

--- arrayEasy1.java
+++ arrayEasy1.java
@@ -1,36 +1,55 @@
 package dsajava;
+
+import java.util.Arrays;
+
 public class arrayEasy1 {
-    public static int majorityElementOp(int nums[]){ // optimized
-        int maj = nums[0];
-        int count = 1;
-        for(int i =1; i<nums.length; i++){
-            if(nums[i]==maj){
-                count++;
-            }else if(count<=0){
-                maj=nums[i];count=1;
-            }else{
-                count--;
-            }
-            
-
-        }
-        return maj;
-
-    }
+    // public static int majorityElementOp(int nums[]){ // optimized
+    //     int maj = nums[0];
+    //     int count = 1;
+    //     for(int i =1; i<nums.length; i++){
+    //         if(nums[i]==maj){
+    //             count++;
+    //         }else if(count<=0){
+    //             maj=nums[i];count=1;
+    //         }else{
+    //             count--;
+    //         }
+    //     }
+    //     return maj;
+    // }
     // public static int majorityElement(int nums[]){ // brute force
     //     int maj = nums.length/2;
     //     Arrays.sort(nums);
     //     return nums[maj];
     //     }
-    public static void main(String args[]){
-     System.out.println("|| Java dsa || Arrays 1 || Questions 1 to 5  ||");
-   
-    int nums[] = {2,2,1,1,1,2,2};
-    System.out.println(majorityElementOp(nums));
-   // System.out.println(majorityElement(nums));
-   
-    
+    public static void merge(int nums1[],int m,int nums2[],int n){
+        int i =m-1;
+        int j = n-1;
+        int idx = (m+n)-1;
+        while(i>=0 & j>=0){
+            if(nums2[j]>nums1[i]){
+                nums1[idx--] = nums2[j--];
+                
+            }else{
+                nums1[idx--]= nums1[i--];
+            }
+        }
+        while(j>=0){
+             nums1[idx--] = nums2[j--];
 
+        }
+      
     }
+    public static void main(String args[]){
+    
+     
+     int nums1[] ={1,2,3,0,0,0};
+     int m =3;
+     int nums2[] ={2,5,6};
+     int n =3;
+     merge(nums1, m, nums2, n);
+    
+       System.out.println(Arrays.toString(nums1));
+       }
     
 }
