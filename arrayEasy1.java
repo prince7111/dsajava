0c596641a7f2ce678dbe5fbb9bae8b5cf1b2588c
prince711 <princemeena7111@gmail.com>
2025-08-12 06:42:20 -0700

    majority element Brute force

--- arrayEasy1.java
+++ arrayEasy1.java
@@ -1,8 +1,24 @@
 package dsajava;
+import java.util.*;
+
 public class arrayEasy1 {
+    public static int majorityElement(int nums[]){
+        int maj = nums.length/2;
+        Arrays.sort(nums);
+        return nums[maj];
+    
+
+
+    }
     public static void main(String args[]){
     System.out.println("hello world");
-    System.out.println("Java dsa|| Arrays D1 || Questions 1 to 5  ||");
+    System.out.println("|| Java dsa|| Arrays 1 || Questions 1 to 5  ||");
+   
+    int nums[] = {2,2,1,1,1,2,2};
+    System.out.println(majorityElement(nums));
+   
+    
+
     }
     
 }
