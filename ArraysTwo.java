5bb472925d4550a9089a06dc804ca4cb3e328986
prince711 <princemeena7111@gmail.com>
2025-08-18 13:38:51 -0700

    four some ||

--- ArraysTwo.java
+++ ArraysTwo.java
@@ -19,81 +19,79 @@ public class ArraysTwo {
     //             map.put(nums[j],j);
     //             }
 
-    //         }
-    //     }
-    //       List<List<Integer>> resList = new ArrayList<>(st);
-    //       return resList;
-    // }
-    // public List<List<Integer>> threesum (int nums[]){
-    //    ArrayList< ArrayList<Integer>> findtriplet = new ArrayList<>();
-    //     // two pointer
-    //     Arrays.sort(nums);
-    //     for(int i =0;i<nums.length;i++){
-    //         int st =i+1;
-    //         int end = nums.length-1;
-    //         if(i>0 && nums[i]==nums[i-1])continue;
-    //         while (st<end) {
-    //             int sum =nums[i]+nums[st]+nums[end];
-    //         if(sum >0){
-    //             end--;
-    //         }else if(sum<0){
-    //             st++;
-    //         }else{
-    //            ArrayList<Integer> currTriplet = new ArrayList<>();
-    //            currTriplet.add(nums[i]); currTriplet.add(nums[st]); currTriplet.add(nums[end]);
-    //            findtriplet.add(currTriplet);
-    //            st++;end--;
+    // //         }
+    // //     }
+    // //       List<List<Integer>> resList = new ArrayList<>(st);
+    // //       return resList;
+    // // }
+    // // public List<List<Integer>> threesum (int nums[]){
+    // //    ArrayList< ArrayList<Integer>> findtriplet = new ArrayList<>();
+    // //     // two pointer
+    // //     Arrays.sort(nums);
+    // //     for(int i =0;i<nums.length;i++){
+    // //         int st =i+1;
+    // //         int end = nums.length-1;
+    // //         if(i>0 && nums[i]==nums[i-1])continue;
+    // //         while (st<end) {
+    // //             int sum =nums[i]+nums[st]+nums[end];
+    // //         if(sum >0){
+    // //             end--;
+    // //         }else if(sum<0){
+    // //             st++;
+    // //         }else{
+    // //            ArrayList<Integer> currTriplet = new ArrayList<>();
+    // //            currTriplet.add(nums[i]); currTriplet.add(nums[st]); currTriplet.add(nums[end]);
+    // //            findtriplet.add(currTriplet);
+    // //            st++;end--;
 
-    //         }
-    //         while(st<end && nums[st]==nums[st-1]){
-    //             st++;
-    //         }
+    // //         }
+    // //         while(st<end && nums[st]==nums[st-1]){
+    // //             st++;
+    // //         }
          
                 
+    // //         }
+    // //     }
+        
+    // //       List<List<Integer>> res= new ArrayList<>(findtriplet);
+    // //       return res;
+
+
+    // // }
+    //  public List<List<Integer>> fourSum(int []nums, int target) {
+    //     HashSet<List<Integer>> uniqTriplet =new HashSet<>();
+    //     int n =nums.length;
+    //     for(int i =0;i<n;i++){
+    //         for(int j =i+1;j<n;j++){
+    //             for(int k =j+1;k<n;k++){
+    //                 for(int l =k+1;l<n;l++){
+    //                     int sum = nums[i]+nums[j]+nums[k]+nums[l];
+    //                     if(sum ==target){
+    //                         ArrayList<Integer> findTriplet =new ArrayList<>();
+    //                         findTriplet.add(nums[i]);
+    //                         findTriplet.add(nums[j]);
+    //                         findTriplet.add(nums[k]);
+    //                         findTriplet.add(nums[l]);
+    //                         Collections.sort(findTriplet);
+    //                         uniqTriplet.add(findTriplet);
+
+    //                     }
+    //                 }
+    //             }
     //         }
     //     }
-        
-    //       List<List<Integer>> res= new ArrayList<>(findtriplet);
-    //       return res;
-
-
-    // }
-     public List<List<Integer>> fourSum(int[] nums, int target) {
-        HashSet<List<Integer>> uniqTriplet =new HashSet<>();
-        int n =nums.length;
-        for(int i =0;i<n;i++){
-            for(int j =i+1;j<n;j++){
-                for(int k =j+1;k<n;k++){
-                    for(int l =k+1;l<n;l++){
-                        int sum = nums[i]+nums[j]+nums[k]+nums[l];
-                        if(sum ==target){
-                            ArrayList<Integer> findTriplet =new ArrayList<>();
-                            findTriplet.add(nums[i]);
-                            findTriplet.add(nums[j]);
-                            findTriplet.add(nums[k]);
-                            findTriplet.add(nums[l]);
-                            Collections.sort(findTriplet);
-                            uniqTriplet.add(findTriplet);
-
-                        }
-                    }
-                }
-            }
-        }
      
-        List<List<Integer>> resultList = new ArrayList<>(uniqTriplet);
-        return resultList;
-    }
+    //     List<List<Integer>> resultList = new ArrayList<>(uniqTriplet);
+    //     return resultList;
+    // }
+    public List<List<Integer>> foursumH(int nums[],int target){
+        ArrayList<ArrayList<Integer>> findiTriplet = new arrayEasy1();
+
+    
+      
     public static void main(String args[]){
-        ArraysTwo sol = new ArraysTwo();
-        int nums[] = {1,0,-1,0,-2,2};
-        int target =0;
-         List<List<Integer>> fourSum = sol.fourSum(nums, target);
-         System.out.println(" four sum List :"+fourSum);
-
-       
-
-
+       int nums[] ={1,0,-1,0,-2,2};
+       int target =0;
     }
     
 }
