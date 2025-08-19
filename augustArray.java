185de843835fd9331154c9a861bb9a3d0eb0097d
prince711 <princemeena7111@gmail.com>
2025-08-19 09:02:06 -0700

    three sum || optimised ||

--- augustArray.java
+++ augustArray.java
@@ -24,10 +24,42 @@ public class augustArray{
     }
     return new ArrayList<>(tripleHashSet);
  }
+ public List<List<Integer>> threesumOptimal(int nums[]){
+    Arrays.sort(nums);
+    List<List<Integer>> result = new ArrayList<>();
+    for(int i =0; i<nums.length;i++){
+        if(i>0 && nums[i]==nums[i-1])continue;
+
+
+        int j =i+1;int k =nums.length-1;
+        while(j<k){
+            int s = nums[i]+nums[j]+nums[k];
+            if(s<0){
+                j++;
+            }else if(s>0){
+                k--;
+
+            }else{
+                List<Integer> ct = new ArrayList<>();
+                ct.add(nums[i]);ct.add(nums[j]);ct.add(nums[k]);
+                result.add(ct);
+                j++;k--;
+                  while(j<k && nums[j]==nums[j-1]){
+                j++;
+            }
+            while(j<k && nums[k]==nums[k-1]){
+                k--;
+            }
+            }
+          
+        }
+    }
+    return new ArrayList<>(result);
+ }
     public static void main(String args[]){
         augustArray sol = new augustArray();
         int nums []= {-1,0,1,2,-1,4};
-        List<List<Integer>> res=sol.threesum(nums);
+        List<List<Integer>> res=sol.threesumOptimal(nums);
         System.out.println(res);
     }
 }
