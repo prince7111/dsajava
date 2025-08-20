a6b11535e147ffed9d00c11afc9229bf8935badd
prince711 <princemeena7111@gmail.com>
2025-08-19 23:00:40 -0700

    D3 : Arrays 3 || next permutation || brute foce ||

--- dsaLesson.java
+++ dsaLesson.java
@@ -1,60 +1,103 @@
 package dsajava;
+import java.util.*;
+
 public class dsaLesson {
-    public static boolean isPalindrome(String str){
-        int st = 0;
-        int end = str.length()-1;
-        while(st<end){
-            if(str.charAt(st)!=str.charAt(end)){
-                return false;
+    // public static boolean isPalindrome(String str){
+    //     int st = 0;
+    //     int end = str.length()-1;
+    //     while(st<end){
+    //         if(str.charAt(st)!=str.charAt(end)){
+    //             return false;
         
-            }
-            st++;end--;
-        }
-        return true;
+    //         }
+    //         st++;end--;
+    //     }
+    //     return true;
   
-    }
-    public static int displacement(String str){
-        int x =0;
-        int y =0;
+    // }
+    // public static int displacement(String str){
+    //     int x =0;
+    //     int y =0;
         
-        for(int i =0; i<str.length();i++){
-            char dir = str.charAt(i);
-            if(dir=='W'){
-                x--;
-            }else if(dir =='N'){
-                y++;
-            }else if(dir =='S'){
-                y--;
-            }else{
-                x++;
-            }
-        }
-        int x_Suare = x*x;
-        System.out.println("x="+x+" x*x="+x_Suare);
-        int y_Suare = y*y;
-        System.out.println("y="+y+" y*y="+y_Suare);
-        return (int)Math.sqrt(x_Suare+y_Suare);
+    //     for(int i =0; i<str.length();i++){
+    //         char dir = str.charAt(i);
+    //         if(dir=='W'){
+    //             x--;
+    //         }else if(dir =='N'){
+    //             y++;
+    //         }else if(dir =='S'){
+    //             y--;
+    //         }else{
+    //             x++;
+    //         }
+    //     }
+    //     int x_Suare = x*x;
+    //     System.out.println("x="+x+" x*x="+x_Suare);
+    //     int y_Suare = y*y;
+    //     System.out.println("y="+y+" y*y="+y_Suare);
+    //     return (int)Math.sqrt(x_Suare+y_Suare);
        
      
 
+    // }
+    // public static String upperCase(String name){
+    //     StringBuilder sb = new StringBuilder("");
+    //     char ch = Character.toUpperCase(name.charAt(0));
+    //     sb.append(ch);
+    //     for(int i =1; i<name.length();i++){
+    //         if(name.charAt(i)==' ' && i<name.length()-1){
+    //             sb.append(name.charAt(i));
+    //             i++;
+    //             sb.append(Character.toUpperCase(name.charAt(i)));
+              
+    //          }else{
+    //                sb.append(name.charAt(i));
+    //          }
+        
+    // }
+    // return sb.toString();
+// }   
+        public static void nextPermutation (int nums[],int n){
+            // 1 2 3 6 5 4
+            int piv =-1;
+            for(int i=n-2;i>=0;i--){
+                if(nums[i]<nums[i+1]){
+                    piv =i; 
+                    break;
+                }
+            }
+            if(piv>0){
+            for(int i= n-1;i>=0;i--){
+                if(nums[i]>nums[piv]){
+                    int temp = nums[piv];
+                    nums[piv] =nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+               
+            }
+        }else{
+            
+            Collections.reverse(Arrays.asList(nums));
+        }
+        Arrays.sort(nums,piv+1,n);
+            
+     
     }
     public static void main(String args[]){
-        String s1 ="apple";
-        String s2 = "mango";
-        String s3 = "banana";
-        int i =0;
-        
-        if(s1.charAt(i)>s2.charAt(i)&&s1.charAt(i)>s3.charAt(i) ){
-            System.out.println("s1 is largest");
-        }else if(s2.charAt(i)>s3.charAt(i)){
-                        System.out.println("s2 is largest");
-            
-        }else{  System.out.println("s3 is largest");
-            
-
+        int nums[] = {1,1,5};
+        int n = nums.length;
+                for(int i =0;i<n;i++){
+            System.out.print(nums[i]+" ");
         }
-        i++;
-      
+        System.out.println();
+            nextPermutation(nums, n);
+        for(int i =0;i<n;i++){
+            System.out.print(nums[i]+" ");
+        
+            
+        }
+        }
     }
     
-}
+
