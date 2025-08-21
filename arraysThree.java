68c5474a0d29349decc7b280f67b9b7964f22b32
prince711 <princemeena7111@gmail.com>
2025-08-21 12:38:32 -0700

    zeroes

--- arraysThree.java
+++ arraysThree.java
@@ -117,10 +117,42 @@ public class arraysThree {
         
         return maxLength;
     }
-    public static void main(String args[]){
-        String s = "abcabcbb";//abcabcbb //bbbb
-        System.out.println("the longest subsubString without repeating "+lengthOfLongestSubstringMap(s));
-      
+      public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        setZeroes(matrix);
+        System.out.println("Zeroes Matrix");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
     
