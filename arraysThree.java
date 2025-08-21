d1f5afa48ea1da04d8dbb0524a1f78d556b2e2da
prince711 <princemeena7111@gmail.com>
2025-08-21 10:58:54 -0700

    || arrays 3 || longest subString || better Using Hashset||

--- arraysThree.java
+++ arraysThree.java
@@ -1,51 +1,109 @@
 package dsajava;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 
 public class arraysThree {
-            public static void nextPermutation (int nums[],int n){
-            // 1 2 3 6 5 4
-            int piv =-1;
-            for(int i=n-2;i>=0;i--){
-                if(nums[i]<nums[i+1]){
-                    piv =i; 
-                    break;
-                }
-            }
-            if(piv>0){
-            for(int i= n-1;i>=0;i--){
-                if(nums[i]>nums[piv]){
-                    int temp = nums[piv];
-                    nums[piv] =nums[i];
-                    nums[i] = temp;
-                    break;
-                }
+    //         public static void nextPermutation (int nums[],int n){
+    //         // 1 2 3 6 5 4
+    //         int piv =-1;
+    //         for(int i=n-2;i>=0;i--){
+    //             if(nums[i]<nums[i+1]){
+    //                 piv =i; 
+    //                 break;
+    //             }
+    //         }
+    //         if(piv>0){
+    //         for(int i= n-1;i>=0;i--){
+    //             if(nums[i]>nums[piv]){
+    //                 int temp = nums[piv];
+    //                 nums[piv] =nums[i];
+    //                 nums[i] = temp;
+    //                 break;
+    //             }
                
-            }
-        }else{
+    //         }
+    //     }else{
             
-            Collections.reverse(Arrays.asList(nums));
-        }
-        Arrays.sort(nums,piv+1,n);
+    //         Collections.reverse(Arrays.asList(nums));
+    //     }
+    //     Arrays.sort(nums,piv+1,n);
             
      
+    // }
+    // public int [][] merger(int intervals[][]){
+    //     // sort array starting index
+    //     Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+    //     int start = intervals[0][0];
+    //     int end = intervals[0][1];
+    //     List<int []> list = new ArrayList<>();
+    //     for(int i=1; i<intervals.length;i++){
+    //         int s = intervals[i][0];
+    //         int e = intervals[i][1];
+    //         if(s<=end){
+    //             end = Math.max(end,e);
+    //         }else{
+    //             list.add(new int[]{start,end});
+    //             start =s;
+    //             end =e;
+    //         }
+    //     }
+    //     list.add(new int[]{start,end});
+    //     return list.toArray(new int [list.size()][]);
+
+
+    // }
+    public static int longestSubStringWIthoutRepeating(String s){
+        int n = s.length();
+        int maxLenght =0;
+        // 
+        for(int i =0; i<n;i++){
+            boolean seen[] = new boolean[128];
+            //
+            for(int j =i; j<n ;j++){
+                char curCH = s.charAt(j);
+                if(seen[curCH]){
+                    break;
+                }else{
+                    seen[curCH]=true;
+                    int curLength = j-i+1;
+                    maxLenght =Math.max(curLength, maxLenght);
+
+                }
+            }
+        }
+        return maxLenght;
+
+    }
+     public static int longestSubStringBetter(String s){
+        int n = s.length();
+        int maxLenght =0;
+        // 
+        for(int i =0; i<n;i++){
+            HashSet<Character> charSet = new HashSet<>();
+            //
+            for(int j =i; j<n ;j++){
+                char curkar = s.charAt(j);
+                if(charSet.contains(curkar)){
+                    break;
+                }else{
+                    charSet.add(curkar);
+                    int curLength = j-i+1;
+                    System.out.print(curLength+" ");
+                    maxLenght =Math.max(curLength, maxLenght);
+
+                }
+            }
+        }
+        return maxLenght;
+
     }
     public static void main(String args[]){
-        int nums[] = {1,2,3,6,5,4};
-        int n = nums.length;
-                for(int i =0;i<n;i++){
-            System.out.print(nums[i]+" ");
-        }
-        System.out.println();
-            nextPermutation(nums, n);
-        for(int i =0;i<n;i++){
-            System.out.print(nums[i]+" ");
-        
-            
-        }
-        }
+        String s = "abcabcbb";//abcabcbb //bbbb
+        System.out.println("the longest subsubString without repeating "+longestSubStringBetter(s));
+      
     }
+}
+    
     
 
 
