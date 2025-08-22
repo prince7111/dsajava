2bf42c187b7c0493259fcb1760c97d475bb3b6b5
prince711 <princemeena7111@gmail.com>
2025-08-21 22:38:59 -0700

    String || compress string ||

--- dsaLesson.java
+++ dsaLesson.java
@@ -98,9 +98,23 @@ public class dsaLesson {
         }
         return sb.toString();
     }
+    public static String compressString (String s){
+        StringBuilder sb = new StringBuilder();
+       
+        for(int i =0;i<s.length();i++){Integer count =1;
+            while(i<s.length()-1 && s.charAt(i)==s.charAt(i+1)){
+                count++;i++;
+            }
+            sb.append(s.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String args[]){
-        String s = "hi my name is prince";
-        System.out.println(upperCase(s));
+       String s = "aaabbcccdd";
+       System.out.println(compressString(s));
        
         }
     }
