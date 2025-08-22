5f163545b932ad33028508117ef3b7d0584b6590
prince711 <princemeena7111@gmail.com>
2025-08-21 22:01:44 -0700

    String || to upper case

--- arraysThree.java
+++ arraysThree.java
@@ -140,19 +140,7 @@ public class arraysThree {
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {
-            {1, 1, 1},
-            {1, 0, 1},
-            {1, 1, 1}
-        };
-        setZeroes(matrix);
-        System.out.println("Zeroes Matrix");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+  
     }
 }
     
--- dsaLesson.java
+++ dsaLesson.java
@@ -84,19 +84,24 @@ public class dsaLesson {
             
      
     }
+    public static String upperCase(String s){
+        int n = s.length();
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(s.charAt(0)); sb.append(ch);
+        for(int i =1; i<n; i++){
+            if(s.charAt(i)==' ' && i<s.length()){
+                sb.append(s.charAt(i)); i++;
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String args[]){
-        int nums[] = {1,1,5};
-        int n = nums.length;
-                for(int i =0;i<n;i++){
-            System.out.print(nums[i]+" ");
-        }
-        System.out.println();
-            nextPermutation(nums, n);
-        for(int i =0;i<n;i++){
-            System.out.print(nums[i]+" ");
-        
-            
-        }
+        String s = "hi my name is prince";
+        System.out.println(upperCase(s));
+       
         }
     }
     
