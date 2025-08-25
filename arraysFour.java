5982d9a7bd0efc9610028a43948eeb327c7bf259
prince711 <princemeena7111@gmail.com>
2025-08-25 09:16:30 -0700

    || Arrays day 4 || Q.2 || find duplicate || brute force ||

--- arraysFour.java
+++ arraysFour.java
@@ -8,11 +8,26 @@ public class arraysFour {
                 cSum +=nums[j];
                 if(cSum ==k){
                     tct++;
+                    break;
                 }
                 
             }
         }
         return tct;
+    }
+    public static int findDuplicate(int nums[]){ 
+        int duplicate = -1;
+        for(int i =0;i<nums.length;i++){
+           
+            
+            for(int j =0; j<nums.length;j++){
+                if(i != j && nums[i]==nums[j]){
+                    duplicate =nums[j];
+
+                }
+            }
+        }
+        return duplicate;
     }
     public static void main(String[] args) {//August
         //Q1 subArry sum eqals K
@@ -20,13 +35,12 @@ public class arraysFour {
         //Q3 Spiral Matrix
         //Q4 Search in Sorted matrix II
         //Q6 Search in Sorted matrix II
-
-        System.out.println("hello world");
-        System.out.println("||java dsa || Arrays 4:");
-        int nums[]= {1,1,1};
-         int k =2;
-         //answer =2;
-         System.err.println(subArraySumEqalsK(nums, k));
+        // System.out.println("hello world");
+        // System.out.println("||java dsa || Arrays 4:");
+        int nums[]= {3,3,3,3,3}; 
+        System.err.println(findDuplicate(nums));
+         
+      
 
     }
     
