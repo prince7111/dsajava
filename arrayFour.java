544d065285b27d6cb076e1b83607d37968f04ea7
prince711 <princemeena7111@gmail.com>
2025-08-25 08:53:56 -0700

    java dsa | Arrays four :

---
+++ arrayFour.java
@@ -0,0 +1,8 @@
+package dsajava;
+public class arrayFour {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("||java dsa || Arrays 4:");
+    }
+    
+}
