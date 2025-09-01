e53fcba2f030dba072b3033cef3bf8f82f919169
prince711 <princemeena7111@gmail.com>
2025-09-01 16:42:09 -0700

    compress string s

--- Strings_chap_2.java
+++ Strings_chap_2.java
@@ -1,4 +1,5 @@
 package dsajava;
+
 public class Strings_chap_2 {
     public static boolean isPalindrome(String str){
         String newStr = "";
@@ -32,12 +33,55 @@ public class Strings_chap_2 {
         double ans =Math.sqrt(x_Suare + y_Suare);
         return ans;
     }
+    public static String large(String fruits[]){
+        String largestString ="";
+        if((fruits[0].compareTo(fruits[1])>0 && (fruits[0].compareTo(fruits[3]))>0)){
+            largestString=fruits[0];
+        }else if(fruits[1].compareTo(fruits[2])>0){
+            largestString=fruits[1];
+        }else{
+        largestString=fruits[2];
+        }
+return largestString;
+    }
+    public static void upperCase(String str){
+            StringBuilder sb = new StringBuilder("");
+        Character ch =str.toUpperCase().charAt(0);
+        sb.append(ch);
+        for(int i=1;i<str.length();i++){
+            ch=str.charAt(i);
+            if(ch==' '&& i<str.length()){
+                sb.append(ch);
+                sb.append(str.toUpperCase().charAt(i+1));
+                i++;
+            }else{
+                sb.append(ch);
+            }
+        }
+    
+System.out.print(sb.toString());
+     
+    }
+    public static String compressTring(String str){
+        String st="";
+        for(int i=0;i<str.length();i++){
+        
+            Integer count=1;
+            while( i<str.length()-1 &&str.charAt(i)==str.charAt(i+1) ){
+                count++;i++;
+            }
+            st+= str.charAt(i);
+            if(count>1){
+                st+=count.toString();
+            }
+        }
+        return st;
+    }
     public static void main(String args[]){
-        System.out.println("Strings in Java dsa || chapete 2,Q.1 || is palindrome");
-        String  Q2 = "WNEENESENNN";
-        System.out.println(findSortPath(Q2));
-
-
+       String str= "aaabbcccdd";
+       System.out.println(compressTring(str));
+       
+    
     }
     
 }
