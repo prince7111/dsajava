3eecac2759682d04efdf2acdb43596aa34e858ec
prince711 <princemeena7111@gmail.com>
2025-09-02 23:36:13 -0700

    binary ones complement

--- chapter_3_Bit_manipulate.java
+++ chapter_3_Bit_manipulate.java
@@ -1,6 +1,9 @@
-package dsajava;
+                                            // ||CHAPETER 3 : bit Maninpulation||  \\\ 
+ package dsajava;
 public class chapter_3_Bit_manipulate {
-    public static void operators_operations(int a,int b){
+    public static void operators_operations(int a,int b){ 
+     System.out.println("Hello World");//java dsa ||chapeter 3 || BIT MANIPULATION || #(pRINCE MEEENA)
+
            //operators 
         //1)AND &
         System.out.print("5 & 6 =");
@@ -11,12 +14,19 @@ public class chapter_3_Bit_manipulate {
         //3) XOR
         System.out.print("5 ^ 6 = ");
         System.out.println(5 ^ 6); //  (3)10
+        //BInary ones compliment
+        System.out.println("BInary ones complement ");
+        System.out.println("~5 ="+~5);
+        //~n =-() || ones complement
+        //~-n()+1 ||2's complement
 
     }
+    
+
     public static void main(String args[]){
+        operators_operations(0,0);
         
-        System.out.println("Hello World");//java dsa ||chapeter 3 || BIT MANIPULATION || #(pRINCE MEEENA)
-       operators_operations(0,0);
+
      
 
     }
