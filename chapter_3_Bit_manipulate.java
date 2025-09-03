13d7b989f7a856e07541ad1f4255e16d7d51cfef
prince711 <princemeena7111@gmail.com>
2025-09-02 22:30:01 -0700

    bitwize operators #1\

--- chapter_3_Bit_manipulate.java
+++ chapter_3_Bit_manipulate.java
@@ -1,8 +1,23 @@
 package dsajava;
 public class chapter_3_Bit_manipulate {
+    public static void operators_operations(int a,int b){
+           //operators 
+        //1)AND &
+        System.out.print("5 & 6 =");
+        System.out.println(5 & 6);//  (4)10
+        //2)OR |
+        System.out.print("5 | 6 = ");
+        System.out.println(5 | 6); // (7)10
+        //3) XOR
+        System.out.print("5 ^ 6 = ");
+        System.out.println(5 ^ 6); //  (3)10
+
+    }
     public static void main(String args[]){
-        System.out.println("Hello World");//java dsa ||chapeter 3 || BIT MANIPULATION || pRINCE MEEENA"
-         
+        
+        System.out.println("Hello World");//java dsa ||chapeter 3 || BIT MANIPULATION || #(pRINCE MEEENA)
+       operators_operations(0,0);
+     
 
     }
     
