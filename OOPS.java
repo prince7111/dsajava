ba8a06506af4fd210d66cae3ce004ec5f2ba97fa
prince7111 <princemeena7111@gmail.com>
2025-09-07 21:23:40 +0530

    Pen Object creation

--- OOPS.java
+++ OOPS.java
@@ -1,7 +1,36 @@
 package dsajava;
-public class OOPS{
-    public static void main(String[] args) {
-        System.out.println("Object Oriented PRogramming #java");
+
+public class OOPS { 
+    public static void main(String args[]){
+        Pen p_1 = new Pen();
+        p_1.setColor("Blue");
+        System.out.println(p_1.color);
+        p_1.tip = 4;
+        System.out.println(p_1.tip);
+        p_1.color = "Yellow";
+        System.out.println(p_1.color);
+
+
+    }
+    
+}
+class Pen{ 
+    //properties of Pen (real life object)
+    String color;    
+    int tip;      
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
     }
 
 }
+ class student{
+    String name;
+    int age;
+    float percentage;
+      void calcPecentage(int p,int m,int c){
+        percentage = (m+p+c)/3;
+      }
+ }
