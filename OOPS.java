5cd776b82b94862e6b8ef0b7023611143f6d79fe
prince7111 <princemeena7111@gmail.com>
2025-09-08 12:21:34 +0530

    geter & seter || concept problem|| get password(to privaate properties of class) using geter seter

--- OOPS.java
+++ OOPS.java
@@ -1,36 +1,62 @@
 package dsajava;
 
 public class OOPS { 
-    public static void main(String args[]){
-        Pen p_1 = new Pen();
-        p_1.setColor("Blue");
-        System.out.println(p_1.color);
-        p_1.tip = 4;
-        System.out.println(p_1.tip);
-        p_1.color = "Yellow";
-        System.out.println(p_1.color);
+    public static void main(String args[]){ 
+        // Pen p1 = new Pen();
+        // p1.setColor("Blue");
+        // System.out.println(p1.getColor());
+        // p1.setTip(5);
+        // System.out.println(p1.getTip());
+        // p1.setColor("Yellow ");
+        // System.out.println(p1.getColor());
+
+     BankAccount myAcc = new BankAccount();
+     myAcc.username = "@prince_meena";
+     System.out.println(myAcc.username);
+       //myAcc.passWord = "fsdjkjk"
+     myAcc.setPassword("@123456");
+      // System.out.println(myAcc.passWord);
+      System.out.println(myAcc.getPassword());
 
 
+                  
     }
     
 }
-class Pen{ 
-    //properties of Pen (real life object)
-    String color;    
-    int tip;      
-    void setColor(String newColor){
-        color = newColor;
-    }
-    void setTip(int newTip){
-        tip = newTip;
-    }
+// class Pen{
+//    private String color;  //  ...[[p[p[i]]]]
+//    private int tip;     //joioiofuif[][pf[ffof]]
+   
+//     String getColor(){ 
+//         return this.color;
+//     }
+//     int getTip(){
+//         return this.tip;
+//     }
+//     //Setter Function
+//     void setColor(String newColor){
+//         this.color = newColor;
+//     }
+//     void setTip(int tip){
+//         this.tip = tip;
+//     }
+// }
+//  class student{
+//     String name;
+//     int age;
+//     float percentage;
+//       void calcPecentage(int p,int m,int c){
+//         percentage = (m+p+c)/3;
+//       }
+//  }
+ class BankAccount{
+   public  String username;
+   private String password;
+   public void setPassword(String password){
+    this.password = password;
 
-}
- class student{
-    String name;
-    int age;
-    float percentage;
-      void calcPecentage(int p,int m,int c){
-        percentage = (m+p+c)/3;
-      }
+   }
+    String getPassword(){
+        return this.password;
+    }
  }
