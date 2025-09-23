81c8dd0ab53f39434d98f721710c81972ac8759d
prince7111 <princemeena7111@gmail.com>
2025-09-23 19:51:17 +0530

    factorial of a number

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,40 @@
 package dsajava;
 public class Recursion {
     //Recursion Funtion (print numbers)
-    public static void printDec(int n){
-        if(n==10){
+    public static void printInc(int n){
+        if(n==1){
         System.out.print(n+" ");
         return;
         }
+       
+        printInc(n-1); 
         System.out.print(n+" ");
-        printDec(n+1);
     }
+    public static int sumN(int n){
+        if(n==1){
+            return 1;
+        }
+        int snm1 = sumN(n-1);
+        int sum = n+snm1;
+        return sum;
+      
+
+    }
+    public static int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    int fnm1 = fact(n-1);
+    int fn = n * fnm1;
+    return fn;
+}
     public static void main(String[] args) {
-        int n =1;
-        printDec(n);
+        int n =5;
+        System.out.println(fact(n));
+       
+        
         
     }
     
 }
+
