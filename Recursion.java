aaf2abd9906902889b8a3c6077288aa387014827
prince7111 <princemeena7111@gmail.com>
2025-09-23 20:03:45 +0530

    firstOccurance

--- Recursion.java
+++ Recursion.java
@@ -47,9 +47,22 @@ public static boolean isSorted(int arr[] ,int i){
     }
 return isSorted(arr, i+1);
 }
+public static int firstOccurance(int arr[],int k,int i){
+    if(i == arr.length-1){
+        return -1;
+    }
+    if(arr[i]==k){
+        return i;
+    }
+    return firstOccurance(arr ,k,i+1);
+
+}
     public static void main(String[] args) {
-       int arr[] = {1,2,3,4};
-       System.out.println(isSorted(arr, 0));
+       int arr[] = {1,2,3,4,5,6,7,5};
+       int k;
+       System.out.println(firstOccurance(arr,5,0));
+
+
        
     
   
