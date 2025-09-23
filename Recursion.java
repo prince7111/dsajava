58f87b5b457bebb1819d2c9bfa85447dfaac6357
prince7111 <princemeena7111@gmail.com>
2025-09-23 21:41:21 +0530

    power

--- Recursion.java
+++ Recursion.java
@@ -70,11 +70,18 @@ public static int lastOccurance(int arr[],int k,int i){
     }
     return isFound;
 }
-
+public static int power(int x ,int n){
+    if(n==0){
+        return 1;
+    }
+    int xnm1 = power(x, n-1);
+    int xn  = x * xnm1;
+    return xn;
+}
 
     public static void main(String[] args) {
-       int arr[] = {1,2,3,4,5,6,7,5};
-       System.out.println(lastOccurance(arr, 5, 0));
+       int x =2;int n =10;
+       System.out.println(power(x, n));
 
 
 
