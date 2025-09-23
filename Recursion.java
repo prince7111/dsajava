1a913b0ad1d39cab2f6d68643e9111dccdf2ec5a
prince7111 <princemeena7111@gmail.com>
2025-09-23 19:58:27 +0530

    check array is Sorted

--- Recursion.java
+++ Recursion.java
@@ -38,9 +38,20 @@ public static int fibonacci(int n){
     return FN;
 
 }
+public static boolean isSorted(int arr[] ,int i){
+    if(i ==arr.length-1){
+        return true;
+    }
+    if(arr[i]> arr[i+1]){
+        return false;
+    }
+return isSorted(arr, i+1);
+}
     public static void main(String[] args) {
-        int n =5;
-        System.out.println(fibonacci(n));
+       int arr[] = {1,2,3,4};
+       System.out.println(isSorted(arr, 0));
+       
+    
   
        
         
