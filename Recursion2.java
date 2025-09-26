273d342c9c23dead139b50c998e2491d0a185c10
prince7111 <princemeena7111@gmail.com>
2025-09-26 14:13:50 +0530

    Recucrios Part2 || assignment q 1 || Brute force ||

--- Recursion2.java
+++ Recursion2.java
@@ -1,47 +1,65 @@
 package dsajava;
 public class Recursion2 {
-    public static int tillingProblem(int n){
-        //Base Cases
-        if(n == 0 || n == 1){
-            return 1;
-        }
-        //kam
-        //varticaly
-        int fnm1 = tillingProblem(n-1);
-        //horizontly
-        int fnm2 = tillingProblem(n-2);
-        int totalWay = fnm1 + fnm2;
-        return totalWay;
-    }
-    public static void removeDuplicates(String str,int idx, StringBuilder sb,boolean map []){
-        if(idx == str.length()){
-            System.out.println(sb);
-            return ;
-        }
-        char currchar = str.charAt(idx);
-        if(map[currchar - 'a']== true){
-            removeDuplicates(str, idx+1, sb, map);
-        }else{
-            map[currchar - 'a'] = true; 
+    // public static int tillingProblem(int n){
+    //     //Base Cases
+    //     if(n == 0 || n == 1){
+    //         return 1;
+    //     }
+    //     //kam
+    //     //varticaly
+    //     int fnm1 = tillingProblem(n-1);
+    //     //horizontly
+    //     int fnm2 = tillingProblem(n-2);
+    //     int totalWay = fnm1 + fnm2;
+    //     return totalWay;
+    // }
+    // public static void removeDuplicates(String str,int idx, StringBuilder sb,boolean map []){
+    //     if(idx == str.length()){
+    //         System.out.println(sb);
+    //         return ;
+    //     }
+    //     char currchar = str.charAt(idx);
+    //     if(map[currchar - 'a']== true){
+    //         removeDuplicates(str, idx+1, sb, map);
+    //     }else{
+    //         map[currchar - 'a'] = true; 
 
-            removeDuplicates(str, idx+1, sb.append(currchar), map);//unique ko add kar lo
-        }
+    //         removeDuplicates(str, idx+1, sb.append(currchar), map);//unique ko add kar lo
+    //     }
 
-    }
-    public static int fPairing(int n){
-        if(n == 1 || n == 2){
-            return n;
-        }
-        int fnm1 =fPairing(n-1);
-        int fnm2 = fPairing(n-2);
-        int totalPairWay = n-1 * (fnm2);
-        int totalPairs = fnm1 + totalPairWay;
-        return totalPairs;
+    // }
+    // public static int fPairing(int n){
+    //     if(n == 1 || n == 2){
+    //         return n;
+    //     }
+    //     int fnm1 =fPairing(n-1);
+    //     int fnm2 = fPairing(n-2);
+    //     int totalPairWay = n-1 * (fnm2);
+    //     int totalPairs = fnm1 + totalPairWay;
+    //     return totalPairs;
         
+    // }
+    public static void allOcc(int arr[],int key,int i,String str){
+        if(i == arr.length){
+            System.out.println(str);
+            return;
+        }
+        if(arr[i]==key){
+            str+=i+" ";
+
+        }
+        allOcc(arr, key, i+1, str);
+      
+
     }
     public static void main(String args[]){
-        int n =3;
-        System.out.println(fPairing(n));
+        String str = "";
+      
+       int arr [] = {3,2,4,5,6,2,7,2};
+       int key =2;
+       allOcc(arr, key, 0, str);
+     
+
       
     }
     
