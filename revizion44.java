c9eaf1c1865d9167516e37d3c6902f27164a03cf
prince7111 <princemeena7111@gmail.com>
2025-10-11 21:38:53 +0530

    friends pair 13(3)

--- revizion44.java
+++ revizion44.java
@@ -1,13 +1,59 @@
 public class revizion44{
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        //java practice And Revision 
+       //java practice And Revision 
         //Recursion II (chapter(Lesson)+Assignment Qustions)
-        /*1)tilling problem
-         * 2)freind pair 
+        /*1)tilling problem (T)
+         * 2)freind pair     (T)
          * 3)binary string problem
          * Assignment q .......
          */
+    public static int tillingProblem(int n){
+        //base case
+        if( n == 0 || n ==1 ){
+            return 1;
+        }
+        //kaam
+        //verticle choice
+        int fnm1 = tillingProblem(n-1);
+        //horizontle choice
+        int fnm2 = tillingProblem(n-2);
+        int toatalWay = fnm1 + fnm2;
+        return  toatalWay;
+        //inner call
+    }
+    public static void rmvDString(String s1,boolean arr[],StringBuilder sb,int i){
+        //case
+        if(i == s1.length()){
+            System.out.println(sb);
+            return;
+        }
+        //kaam
+        char currentCharacter = s1.charAt(i);
+        if(arr[currentCharacter - 'a']==true){
+            //calling
+            rmvDString(s1,arr,sb,i+1);
+        }else{
+            arr[currentCharacter - 'a']=true;
+            //calling
+            rmvDString(s1,arr,sb.append(currentCharacter),i+1);
+        }
+        
+
+    }
+    public static int friendsPair(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        int fnm1 = friendsPair(n-1);
+        int fnm2 = friendsPair(n-2);
+        int parWay = (n-1) * fnm2;
+        int totalWay = parWay + fnm1;
+        return totalWay;
+    }
+    public static void main(String args[]){
+     int n =4;
+     System.out.println(friendsPair(n))  ; 
+        
+     
 
     }
 }
