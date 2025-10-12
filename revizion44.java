b711ab1c1567684bef0a7bf34354223e7c924f72
prince7111 <princemeena7111@gmail.com>
2025-10-12 20:41:17 +0530

    recursion(II) Q 4

--- revizion44.java
+++ revizion44.java
@@ -103,12 +103,28 @@ public class revizion44{
         return lengthOfString(str, lenght, idx+1);
         
     }
+    public static int effclengthOfString(String str,int i){
+        if(i==str.length()){
+            return 0;
+        }
+        return  1 + effclengthOfString(str, i+1);
+    }
+    public static int countSubstring(String str,int i,int j,int n){
+        if(n==1){return 1;}
+        if(n<=0){return 0;}
+        int res = countSubstring(str, i+1, j, n-1) +
+                  countSubstring(str, i, j-1, n-1)-
+                  countSubstring(str, i+1, j-1, n-2);
+                  if(str.charAt(i)==str.charAt(j)){
+                    res++;
+                  }
+                  return res;
+    }
     public static void main(String args[]){
-        String str = "prince meena";
-        System.out.println( lengthOfString(str, 0,0));
+        String str = "abcab";
+        int n = str.length();
+        System.out.println( countSubstring(str, 0, n-1,n ));
       
-    
-     
-
+      
     }
 }
