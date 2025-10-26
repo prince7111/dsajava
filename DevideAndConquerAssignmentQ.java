412956b79d6d2d44a9c625a44afdc6911fd0c2bc
prince7111 <princemeena7111@gmail.com>
2025-10-26 16:01:51 +0530

    ||Devide & Conqer || majority elements||

--- DevideAndConquerAssignmentQ.java
+++ DevideAndConquerAssignmentQ.java
@@ -74,16 +74,41 @@ public class DevideAndConquerAssignmentQ {
             return false;
         }
     }
-    public static void main(String args[]){
-       String []arr = {"sun", "earth", "mars", "mercury"};
-    // mergeSortLownHigh(arr, 0, arr.length-1);
-    // for(int i =0;i<arr.length;i++){
-    //     System.out.print(arr[i]+" ");
-    // }
-    String []a = mergeSortLownHigh(arr, 0, arr.length-1);
-    for(int i =0;i<arr.length;i++){
-        System.out.print(a[i]+" ");
+    public static int countInRange(int nums[],int num,int lo,int hi){
+        int count =0;
+        for(int i =lo; i<=hi;i++){
+            if(nums[i]==num){
+                count++;
+            }
+
+        }
+        return count;
     }
+    public static int majorityElementRec(int nums[],int lo,int hi){
+        //base case
+        if(lo==hi){
+            return nums[lo];
+        }
+        int mid = lo+(hi-lo)/2;
+        int left = majorityElementRec(nums, lo, mid);
+        int right = majorityElementRec(nums,mid+1,hi);
+        //
+        if(left == right){
+            return left;
+        }
+        int leftCount = countInRange(nums,left,lo,hi);
+        int rightCount = countInRange(nums,right,lo,hi);
+        return leftCount>rightCount ?  left :right;
+
+    }
+        public static int majorityElement(int nums[]){
+            return majorityElementRec(nums, 0, nums.length-1);
+        
+    }
+    public static void main(String args[]){
+       int nums[] = {2,2,1,1,1,2,2};
+       System.out.println(majorityElement(nums));
+       
 
 
 
