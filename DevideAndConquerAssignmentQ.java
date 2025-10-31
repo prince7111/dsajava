4f5309e1fec0a80136ff82ff04d4df19c38da042
prince7111 <princemeena7111@gmail.com>
2025-10-31 20:14:36 +0530

    ||Java Porjects || (3) Reverse String ||

--- DevideAndConquerAssignmentQ.java
+++ DevideAndConquerAssignmentQ.java
@@ -1,114 +1,155 @@
 public class DevideAndConquerAssignmentQ {
-    //string sorting (using merge sort)
-    public static void print(String arr[]){
-        for(int i =0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-    public static void mergeSort(String arr[],int si,int ei){
-        if(si >= ei){
-            return;
-        }
-        int mid = si+(ei-si)/2;
-        mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
-        merge(arr,si,mid,ei);
-    }
-     public static void merge(String arr[],int si,int mid,int ei){
-        String temp[] = new String[(ei-si)+1];
-        int i =si;int j = mid+1;int k=0;
-        while(i<=mid && j<=ei){
-            if(arr[i].compareTo(arr[j])==-1){
-                temp[k++] = arr[i++];
-            }else{
-                  temp[k++] = arr[j++];
-            }
-        }
-        while(i<=mid){
-              temp[k++] = arr[i++];
-        }
-        while(j<=ei){
-            temp[k++] = arr[i++];
-        }
-        for(i = si,k=0;k<temp.length;k++,i++){
-            arr[i]= temp[k];
-        }
+//     //string sorting (using merge sort)
+//     public static void print(String arr[]){
+//         for(int i =0; i<arr.length; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+//     public static void mergeSort(String arr[],int si,int ei){
+//         if(si >= ei){
+//             return;
+//         }
+//         int mid = si+(ei-si)/2;
+//         mergeSort(arr, si, mid);
+//         mergeSort(arr, mid+1, ei);
+//         merge(arr,si,mid,ei);
+//     }
+//      public static void merge(String arr[],int si,int mid,int ei){
+//         String temp[] = new String[(ei-si)+1];
+//         int i =si;int j = mid+1;int k=0;
+//         while(i<=mid && j<=ei){
+//             if(arr[i].compareTo(arr[j])==-1){
+//                 temp[k++] = arr[i++];
+//             }else{
+//                   temp[k++] = arr[j++];
+//             }
+//         }
+//         while(i<=mid){
+//               temp[k++] = arr[i++];
+//         }
+//         while(j<=ei){
+//             temp[k++] = arr[i++];
+//         }
+//         for(i = si,k=0;k<temp.length;k++,i++){
+//             arr[i]= temp[k];
+//         }
 
-    }
-   public static String[] mergeSortLownHigh(String arr[],int lo,int hi){
-    //base case
-    if(lo==hi){
-        String A[] = {arr[lo]};
-        return A;
-    }
-    //kam
-    int mid = lo+(hi-lo)/2;
-    String arr1 [] = mergeSortLownHigh(arr, lo, mid);
-    String arr2 [] = mergeSortLownHigh(arr, mid+1, hi);
-    String arr3[]  = mergee(arr1,arr2);
-    return arr3;
-   }
-    public static String [] mergee(String arr1[],String arr2[]){
-        int n = arr1.length;int m = arr2.length;
-        String arr3[] = new String[n+m];
-        int idx = 0;int i =0;int j=0;
-        while(i<n && j<m){
-            if(isAlphabatticallySmaaller((arr1[i]), (arr2[j]))){
-                 arr3[idx++] = arr1[i++];
-            }else{
-                arr3[idx++] = arr2[j++];   
-            }
-        }
-        while(i<n){
-              arr3[idx++] = arr1[i++];
-        }
-        while (j < m) {
-              arr3[idx++] = arr2[j++];           
-        }
-        return arr3;
-    }
-    public static boolean  isAlphabatticallySmaaller(String str1,String str2){
-        if(str1.compareTo(str2)<0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public static int countInRange(int nums[],int num,int lo,int hi){
-        int count =0;
-        for(int i =lo; i<=hi;i++){
-            if(nums[i]==num){
-                count++;
-            }
+//     }
+//    public static String[] mergeSortLownHigh(String arr[],int lo,int hi){
+//     //base case
+//     if(lo==hi){
+//         String A[] = {arr[lo]};
+//         return A;
+//     }
+//     //kam
+//     int mid = lo+(hi-lo)/2;
+//     String arr1 [] = mergeSortLownHigh(arr, lo, mid);
+//     String arr2 [] = mergeSortLownHigh(arr, mid+1, hi);
+//     String arr3[]  = mergee(arr1,arr2);
+//     return arr3;
+//    }
+//     public static String [] mergee(String arr1[],String arr2[]){
+//         int n = arr1.length;int m = arr2.length;
+//         String arr3[] = new String[n+m];
+//         int idx = 0;int i =0;int j=0;
+//         while(i<n && j<m){
+//             if(isAlphabatticallySmaaller((arr1[i]), (arr2[j]))){
+//                  arr3[idx++] = arr1[i++];
+//             }else{
+//                 arr3[idx++] = arr2[j++];   
+//             }
+//         }
+//         while(i<n){
+//               arr3[idx++] = arr1[i++];
+//         }
+//         while (j < m) {
+//               arr3[idx++] = arr2[j++];           
+//         }
+//         return arr3;
+//     }
+//     public static boolean  isAlphabatticallySmaaller(String str1,String str2){
+//         if(str1.compareTo(str2)<0){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+//     public static int countInRange(int nums[],int num,int lo,int hi){
+//         int count =0;
+//         for(int i =lo; i<=hi;i++){
+//             if(nums[i]==num){
+//                 count++;
+//             }
 
-        }
-        return count;
-    }
-    public static int majorityElementRec(int nums[],int lo,int hi){
-        //base case
-        if(lo==hi){
-            return nums[lo];
-        }
-        int mid = lo+(hi-lo)/2;
-        int left = majorityElementRec(nums, lo, mid);
-        int right = majorityElementRec(nums,mid+1,hi);
-        //
-        if(left == right){
-            return left;
-        }
-        int leftCount = countInRange(nums,left,lo,hi);
-        int rightCount = countInRange(nums,right,lo,hi);
-        return leftCount>rightCount ?  left :right;
+//         }
+//         return count;
+//     }
+//     public static int majorityElementRec(int nums[],int lo,int hi){
+//         //base case
+//         if(lo==hi){
+//             return nums[lo];
+//         }
+//         int mid = lo+(hi-lo)/2;
+//         int left = majorityElementRec(nums, lo, mid);
+//         int right = majorityElementRec(nums,mid+1,hi);
+//         //
+//         if(left == right){
+//             return left;
+//         }
+//         int leftCount = countInRange(nums,left,lo,hi);
+//         int rightCount = countInRange(nums,right,lo,hi);
+//         return leftCount>rightCount ?  left :right;
 
-    }
-        public static int majorityElement(int nums[]){
-            return majorityElementRec(nums, 0, nums.length-1);
+//     }
+//         public static int majorityElement(int nums[]){
+//             return majorityElementRec(nums, 0, nums.length-1);
         
+//     }
+    public static int merge(int arr[],int left,int mid,int right){
+        int  invCount =0;
+        int i =left;
+        int j =mid;
+        int k=0;
+        int tem[] = new int [(right - left)+1];
+        while( i < mid && j <= right){
+            if(arr[i]< arr[j]){
+                tem[k++] = arr[i++];
+
+            }else{
+                tem[k++] = arr[j++];
+                invCount += (mid-i);
+            }
+        }
+        while( i < mid){
+             tem[k++] = arr[i++];
+        }
+        while(j <= right){
+             tem[k++] = arr[j++];
+        }
+        for(i=left,k=0;k<tem.length;k++,i++){
+            arr[i] = tem[k];
+        }
+        return invCount;
+    }
+    private  static int mergSort(int arr[],int left,int right){
+        int invCount =0;
+        if(right > left){
+            int mid = left+(right-left)/2;
+            invCount = mergSort(arr, left, mid);
+            invCount+= mergSort(arr, mid+1, right);
+            invCount+= merge(arr, left, mid+1,right);
+        }
+        return invCount;
+
+    }
+    public static int getInversion(int arr[]){
+        int n = arr.length;
+        return mergSort(arr, 0, n-1);
     }
     public static void main(String args[]){
-       int nums[] = {2,2,1,1,1,2,2};
-       System.out.println(majorityElement(nums));
-       
+        //inversion count (n*logn)
+    int arr[] ={2, 4, 1, 3, 5};
+    System.err.println(getInversion(arr));
 
 
 
--- Ppattern.java
+++ Ppattern.java
@@ -1,7 +1,7 @@
+package dsajava;
 public class Ppattern {
-    public static void main(String[] args) {
-        int n =3;
-        for(int i =0;i<n;i++){
+    public static void patterP(int n){
+         for(int i =0;i<n;i++){
             for(int j =0;j<n;j++){
                 if(j==0 || (i<=(n/2)&& j==n/2)){
                     System.out.print(" * ");
@@ -11,6 +11,10 @@ public class Ppattern {
             }
             System.err.println();
         }
+    }
+    public static void main(String[] args) {
+        int n =3;
+    patterP(n);
     }
     
 }
