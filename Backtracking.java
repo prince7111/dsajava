22f715509aa8f45824b4c1a6c3fe6699387828e3
prince7111 <princemeena7111@gmail.com>
2025-11-04 14:02:34 +0530

    ||BackTraking ||Q.1||changeArr ||

--- Backtracking.java
+++ Backtracking.java
@@ -1,7 +1,30 @@
 package dsajava;
 public class Backtracking {
+
+    //Question 1: backrraking arrays
+    public static void changeArr(int arr[],int i,int val){
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        //winding & recurion
+        arr[i] = val;        
+        changeArr(arr, i+1, val+1);
+        //baktracking & unwinding
+        arr[i] = arr[i]-2;              
+    }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.err.println();
+    }
     public static void main(String args[]){
         System.out.println("Hello world,coding 4 Nov,2025");
         System.out.println("Backtracking");
+        int arr[] = new int[10];
+        changeArr(arr, 0,1);
+        printArr(arr);
     }
 }
