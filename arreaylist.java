aeacd2c69c512d616b63577f54d68c9884ef6a29
prince7111 <princemeena7111@gmail.com>
2025-11-08 21:31:43 +0530

    Arraylist || file

---
+++ arreaylist.java
@@ -0,0 +1,20 @@
+package dsajava;
+import java.util.*;
+public class arreaylist {
+    public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>();
+        ArrayList <String> list2 = new ArrayList<>();
+        ArrayList <Double> list3 = new ArrayList<>();
+        //Operation 
+        //add O(1)
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+     System.out.println(list.contains(4));
+
+        
+
+    }
+    
+}
