0583b29dd26c364d35a4ece3f9156d783e9928f8
prince7111 <princemeena7111@gmail.com>
2025-11-08 22:18:33 +0530

    reverse String

--- arreaylist.java
+++ arreaylist.java
@@ -1,21 +1,21 @@
 package dsajava;
 import java.util.*;
 public class arreaylist {
+   public static void rEverseArrList(ArrayList<Integer>list){
+    for(int i=list.size()-1;i>=0;i--){
+        System.out.print(list.get(i)+" ");
+    }
+   }
     public static void main(String[] args) {
         ArrayList <Integer> list = new ArrayList<>();
-        ArrayList <String> list2 = new ArrayList<>();
-        ArrayList <Double> list3 = new ArrayList<>();
-        //Operation 
-        //add O(1)
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
-     System.out.println(list);
-     for(int i =0;i<list.size();i++){
-        System.err.print(list.get(i)+" ");
-     }
-     System.out.println();
+        rEverseArrList(list);
+
+       
+    
 
         
 
