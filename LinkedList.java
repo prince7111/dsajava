cd7c59fd9705afd158924dca28cc4897045c66b6
prince7111 <princemeena7111@gmail.com>
2025-11-21 19:05:44 +0530

    LinkedList || add in middle

--- LinkedList.java
+++ LinkedList.java
@@ -34,22 +34,33 @@ public class LinkedList{
         }
         Node temp = head;
         while(temp != null){
-            System.out.print(temp.data+" ");
+            System.out.print(temp.data+"->");
             temp = temp.next;
         }
-        System.out.println("null");
+           System.out.println("null");
     }
+        public void add(int idx,int data){
+            Node newNode = new Node(data);
+            Node temp = head;
+            int i = 0;
+            while(i<=idx-2){
+                temp = temp.next;
+                i++;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
-            ll.print();
         ll.addFirst(2);
-            ll.print();
         ll.addFirst(1);
-            ll.print();
         ll.addLast(3);
-            ll.print();
         ll.addLast(4);
+        ll.add(2, 9);
         ll.print();
+
+        
         
          
     }
