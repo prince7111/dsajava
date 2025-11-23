2529f934209d29784f864a192291fc1eb505bf30
prince7111 <princemeena7111@gmail.com>
2025-11-23 13:26:14 +0530

    Stacks In Java File..

---
+++ StackB.java
@@ -0,0 +1,8 @@
+public class StackB {
+    public static void main (String args[]){
+        System.out.println("hell0 wold");
+        System.err.println("file 1; Stacks in java..");
+       
+    }
+    
+}
