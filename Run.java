71deb7f5ae7559a006e0a90890d7bd311e36c4a1
prince7111 <princemeena7111@gmail.com>
2025-11-24 16:38:24 +0530

    Linked list code run 1

---
+++ Run.java
@@ -0,0 +1,121 @@
+public class Run{
+    //CLASS LINKED LSIT
+    public static class Node{ //CLASS NODE
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    } //PORPERTEIS OF MAIN CLASS (LINKED LIST)
+    public static  Node head;
+    public static  Node tail;
+    public static  int size;
+    //addFirs() 
+    public void addFirst(int data){
+        Node newNode = new Node(data);//newNode
+        size++;
+        
+        if(head ==null){
+            head = tail = newNode;  
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    // method 2 addLast()
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+           head = tail =newNode;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    //3 removeFirst
+    public int removeFirst(){
+        if(head ==null){
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        }else if(size ==1){
+            head = tail = null;
+            size =0;
+        
+        }
+        int val = head.data;
+        head = head.next; //delete Node
+        size--;
+        return val;
+    }
+    //4 removelast
+    public int removeLast(){
+        if(head == null){
+            System.out.println("LL is Empty");
+            return Integer.MAX_VALUE;
+        }else if(size ==1){
+            int val = head.data;
+            head = tail = null;
+            size =0;
+            return val;
+
+        }
+        Node prev = head;
+       for(int i=0;i<size-2;i++){
+        prev = prev.next;
+       }
+        int val = prev.next.data;
+        prev.next =null;
+        tail = prev;
+        size--;
+        return val;
+    }//Prining a linked list
+    public void print(){
+        if(head == null){
+            System.out.println("LL is Empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+        System.out.print(temp.data+"--");
+      
+        temp = temp.next;
+        }
+         System.out.println("null");
+    }
+    public static int serachKey(int key){
+        Node temp = head;
+        int pos =0;
+        while(temp != null){
+            if(temp.data == key){
+                return pos;
+            }
+            temp = temp.next;
+            pos++;
+        }    
+        return -1;
+    }
+    public static void main(String args[]){
+        Run ll = new Run();
+       ll.addFirst(2);
+       ll.addFirst(1);
+       ll.addLast(3);
+       ll.addLast(4);
+       ll.print();
+       System.out.print(  serachKey(4));
+       
+    
+    
+       
+    }
+}
+    
+
+        
+        
+        
+        
+        
+
+        
+      
--- StackB.java
+++ StackB.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+//import java.util.*;
 public class StackB {
+
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        //method()1 isempty():
+        public static boolean isempty(){
+            return (list.size()==0);
+        }
+        //push
+        public static void push(int data){
+            list.add(data);
+
+        }
+    }
     public static void main (String args[]){
-        System.out.println("hell0 wold");
-        System.err.println("file 1; Stacks in java..");
+      
        
     }
     
