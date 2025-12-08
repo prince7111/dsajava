ab98e8165d547e0b65b86383e16b83b08487715e
prince7111 <princemeena7111@gmail.com>
2025-12-08 16:43:12 +0530

    >

--- Stack_Part2.java
+++ Stack_Part2.java
@@ -28,9 +28,31 @@ public class Stack_Part2{
             return false;
         }
     }   
+    public static boolean isDuplicate(String str){
+        Stack <Character> s = new Stack<>();
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch ==')'){
+                int count = 0;
+                while(s.peek()!= '('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
     public static void main(String args[]){
-        String str = "{}";
-        System.out.println( isValidPar(str));
+        String str = "(((a+(b)))+(c+d))";
+        String str2 = "((a+b)+(c+d))";
+        System.out.println( isDuplicate(str));
         
         
     }   
