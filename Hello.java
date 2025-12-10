156f53cc32cf18b066442a174f4affb363e11d04
prince7111 <princemeena7111@gmail.com>
2025-12-10 06:06:32 +0530

    hello 10 dec 2025

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 package dsajava;
     public class Hello{
         public static void main(String args[]){
-            System.out.println("Hello World:dsa+java");
-            System.out.println("Hello guys wlc to december 2025");
-            System.out.println("Let's Start New");
-            //PRINCE MEENA 44 // 2025 dec
+            System.out.println("Hello 10 dec");
         }
     }
