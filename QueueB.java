1cb6fd9ca005d302bb15319a6e618625c2771b1e
prince7111 <princemeena7111@gmail.com>
2025-12-11 19:50:59 +0530

    Queue ||circularQueuUsingArray || 32 Queuue

--- QueueB.java
+++ QueueB.java
@@ -1,61 +1,119 @@
 
-public class QueueB{
-    static class Queue{
-        static int arr[];
-        static int size;
-        static int rear;
-        Queue(int n){
-            size = n;
-            arr = new int[size];
-            rear = -1;
+import javax.rmi.ssl.SslRMIClientSocketFactory;
 
-        } 
-        public static boolean isEmpty(){
-            return rear==-1;
+
+public class QueueB{
+    //array based normal queue
+    // static class Queue{
+    //     static int arr[];
+    //     static int size;
+    //     static int rear;
+    //     Queue(int n){
+    //         size = n;
+    //         arr = new int[size];
+    //         rear = -1;
+
+    //     } 
+    //     public static boolean isEmpty(){
+    //         return rear==-1;
+    //     }
+    //     public static void add(int data) {
+    //         if(rear == size-1){
+    //             System.err.println("queue is full(overFlow)");
+    //             return;
+    //         }
+    //         rear = rear+1; //-1idx==> 0 idx
+    //         arr[rear] = data;
+    //     }
+    //     public static int remove (){
+    //         if(isEmpty()){
+    //             System.out.println("Queue is empty(underFlow)");
+    //             return -1;
+    //         }
+    //         int rmvFront = arr[0];
+    //         for(int i=0;i<rear;i++){
+    //             arr[i] = arr[i+1];
+    //         }
+    //         rear = rear-1;
+    //         return rmvFront;
+    //     }
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             System.err.println("Queue is empty(underFlow)");
+    //             return -1;
+    //         }
+    //         return arr[0];
+    //     }
+    // }
+    //circular Queue
+    static class Queue{
+       static  int arr[];
+       static  int capacity;
+       static  int rear;
+       static  int front;
+        Queue(int size){
+            arr = new int[size];
+            capacity = size;
+            rear = -1;
+            front = -1;
         }
-        public static void add(int data) {
-            if(rear == size-1){
-                System.err.println("queue is full(overFlow)");
+        //empty
+        public static boolean isEmpty(){
+            return front ==-1 || rear==-1;
+        }
+        //full
+        public static boolean isFull(){
+            return ((rear+1)%capacity)==front;
+        }
+        //add
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("Queue is full (Overflow)");
                 return;
             }
-            rear = rear+1; //-1idx==> 0 idx
+            if(front ==-1){
+                front =0;
+            }
+            rear = (rear+1)%capacity;
             arr[rear] = data;
         }
-        public static int remove (){
+        public static int remove(){
             if(isEmpty()){
-                System.out.println("Queue is empty(underFlow)");
+                System.out.println("queue is empty (underflow");
                 return -1;
             }
-            int rmvFront = arr[0];
-            for(int i=0;i<rear;i++){
-                arr[i] = arr[i+1];
+            int removeFront = arr[front];
+            if(front == rear){
+                rear = front = -1;
+            }else{
+                front = (front+1)% capacity;
             }
-            rear = rear-1;
-            return rmvFront;
+            return removeFront;
         }
         public static int peek(){
-            if(isEmpty()){
-                System.err.println("Queue is empty(underFlow)");
-                return -1;
-            }
-            return arr[0];
+           if(isEmpty()){
+            System.out.println("queue is empty(underflow)");
+            return -1;
+           }
+           return arr[front];
         }
     }
     public static void main(String args[]){
-        Queue q = new Queue(8);
-        q.add(2);
-        q.add(4);
-        q.add(6);
-        q.add(8);
-        q.add(10);
-        q.add(12);
-        q.add(14);
-        q.add(16);
+       Queue q = new Queue(3);
+         System.out.println("The capacity of array to simulate Queue"+q.capacity);
+       q.add(1);
+       q.add(2);
+       q.add(3);
+       System.out.println(q.remove());
+       q.add(4);
+       System.out.println(q.remove());
+       q.add(5);
+
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
         }
-        System.out.println("Array base normal queue");
+        System.out.println("better Circualr queue");
 
     }
 }
