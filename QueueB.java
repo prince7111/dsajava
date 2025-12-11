5bcafc8c56d3dc3a872592b0bcc5124f1b6f9c57
prince7111 <princemeena7111@gmail.com>
2025-12-11 13:58:38 +0530

    32) QUEUE || ArrayBaseQueue || normal array based Queue || 11|12\2025

--- QueueB.java
+++ QueueB.java
@@ -1,17 +1,61 @@
-import java.util.*;
+
 public class QueueB{
-    class Queue{
-       static int arr[];
-       static int size;
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        Queue(int n){
+            size = n;
+            arr = new int[size];
+            rear = -1;
 
-        public Queue(int n) {
-        
-
+        } 
+        public static boolean isEmpty(){
+            return rear==-1;
         }
-       
-        
+        public static void add(int data) {
+            if(rear == size-1){
+                System.err.println("queue is full(overFlow)");
+                return;
+            }
+            rear = rear+1; //-1idx==> 0 idx
+            arr[rear] = data;
+        }
+        public static int remove (){
+            if(isEmpty()){
+                System.out.println("Queue is empty(underFlow)");
+                return -1;
+            }
+            int rmvFront = arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i] = arr[i+1];
+            }
+            rear = rear-1;
+            return rmvFront;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.err.println("Queue is empty(underFlow)");
+                return -1;
+            }
+            return arr[0];
+        }
     }
     public static void main(String args[]){
-       
+        Queue q = new Queue(8);
+        q.add(2);
+        q.add(4);
+        q.add(6);
+        q.add(8);
+        q.add(10);
+        q.add(12);
+        q.add(14);
+        q.add(16);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+        System.out.println("Array base normal queue");
+
     }
 }
