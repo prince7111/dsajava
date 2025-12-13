e052ecfafbb641a44f1755b89b2774719955b0ac
prince7111 <princemeena7111@gmail.com>
2025-12-13 18:44:31 +0530

    Queue || 13|12| || 32 queue || Stack using queue

--- QueueB.java
+++ QueueB.java
@@ -1,5 +1,4 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 public class QueueB{                   //Princemeena (Main class)
     //array based normal queue
     // static class Queue{
@@ -137,16 +136,57 @@ public class QueueB{                   //Princemeena (Main class)
     //         }
     //         return head.data;
     //     }
+    static class Queue{
+      static Stack<Integer> s1 = new Stack<>();
+      static Stack<Integer> s2 = new Stack<>();
+
+      public static boolean isEmpty() {
+        return s1.isEmpty();
+      }
+      //add
+      public static void add/*Enque( */(int data){
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(data);
+        while(!s2.isEmpty()){
+          s1.push(s2.peek());
+          s2.pop();
+        }
+      }
+      //remove 
+      public static int remove/*Dequeue*/(){
+        if(isEmpty()){
+          System.out.println("Queue is empty");
+          return -1;
+        }
+        return s1.pop();
+      }
+      //peek
+      public static int peek(){
+        if(isEmpty()){
+          System.out.println("Queue is empty");
+          return -1;
+        }
+        return s1.peek();
+      }
+    }
     
     public static void main(String args[]){
       // QueueB q = new QueueB();
-      Queue<Integer> q = new LinkedList<>();
+      Queue q = new Queue();
        q.add(1);
        q.add(2);
        q.add(3);
+       System.out.println(q.remove());
+       q.add(4);
+       System.out.println(q.remove());
+      
+       q.add(5);
        while(!q.isEmpty()){
-        System.err.println(q.peek());
-        q.remove();
+        System.err.println(q.remove());
+      
        }
+       System.out.println(q.isEmpty());
     }
 }
