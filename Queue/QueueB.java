dcbf0388b39847930dfc5eaa2e96cc87e6918cba
prince7111 <princemeena7111@gmail.com>
2025-12-16 14:02:04 +0530

    deueu || 16|12} || 32 Quqeue ||

--- Queue/QueueB.java
+++ Queue/QueueB.java
@@ -299,19 +299,14 @@ public class QueueB{                   //Princemeena (Main class)
       System.out.println(q1);
   }
     public static void main(String args[]){
-      Queue<Integer> q1 = new LinkedList<>();
-      q1.add(1);
-      q1.add(2);
-      q1.add(3);
-      q1.add(4);
-      q1.add(5);
-      q1.add(6);
-      q1.add(7);
-      q1.add(8);
-      q1.add(9);
-      q1.add(10);
-      System.out.println(q1);
-      interLeave(q1);
+     
+      Deque<Integer> deque= new LinkedList<>(); 
+      deque.addFirst(1);
+      deque.addFirst(2);
+      System.out.println(deque);
+      deque.removeFirst();
+      System.out.println(deque);
+      
      
       
     
