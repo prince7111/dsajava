806bd3eb2379556baeb9dcac1c65637e402dd096
prince7111 <princemeena7111@gmail.com>
2025-12-18 18:15:40 +0530

    Greedy || 33 greedy alogrithm ||

---
+++ Greedy/First.java
@@ -0,0 +1,12 @@
+package dsajava.Greedy;
+//"Prince Meena" java+dsa (III sem)
+//(33) "Greedy Algorithm"
+// 18|12|25 dec 2025
+//
+public class First {
+    public static void main(String args[]){
+        System.out.println("Hello Class");
+
+    }
+    
+}
