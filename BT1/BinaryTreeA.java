6c0eb18f98b1c6844c6846cd88505cca544d1e5b
prince7111 <princemeena7111@gmail.com>
2025-12-26 11:46:48 +0530

    Binarytree-P1 | 34.Binary Tree | Prince Meena 44| 26|12|2025 | lets start for succes milestrone geit

---
+++ BT1/BinaryTreeA.java
@@ -0,0 +1,10 @@
+package BT1;
+
+public class BinaryTreeA {
+    public static void main(String args[]){
+        System.out.println("Hello World@44");
+        System.out.println("My Name is Prince Meena ");
+        System.out.println("Current dsa opic BInary Tree (Part 1");
+    }
+    
+}
