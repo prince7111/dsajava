0e6a33b5c1fc437c1b8ba34035204653c004d0c2
prince7111 <princemeena7111@gmail.com>
2025-12-27 14:22:56 +0530

    BinaryTree | 34binarytree | Traversl (a) preOrder Traversal

--- BT1/BinaryTreeA.java
+++ BT1/BinaryTreeA.java
@@ -23,10 +23,18 @@ public class BinaryTreeA {
             return newNode;
         }
     }
+    public static void preOrderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
     public static void main(String args[]){
        int nodes[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
        BinaryTree tree = new BinaryTree();
        Node root = tree.buildTree(nodes);
-       System.out.println(root.data);
+       preOrderTraversal(root);
     }   
 }
