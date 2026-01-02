e91148cab61ad81f72d979f6de2f90879c00a604
prince7111 <princemeena7111@gmail.com>
2026-01-02 14:18:16 +0530

    BinaryTree |35binaryTree | diameter of tree (aproch 1)

---
+++ BT/BinaryTreeA.java
@@ -0,0 +1,66 @@
+package BT;
+
+import java.util.LinkedList;
+
+public class BinaryTreeA {
+    static class Node{
+       int data;
+       Node left;
+       Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+    public static void preOrderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+          System.out.print(root.data+" ");
+        inorder(root.right);
+
+    }
+    public static void postorder(Node root){
+        if(root == null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void levelorder(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Integer> q = new LinkedList<>();
+    }
+    public static void main(String args[]){
+       int nodes[] ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+       BinaryTree tree = new BinaryTree();
+       Node root = tree.buildTree(nodes);
+    
+    }   
+}
