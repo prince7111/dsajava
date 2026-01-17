109b8e5e1a5170398a449c1668faefb8c9b4c23e
prince7111 <princemeena7111@gmail.com>
2026-01-17 15:36:01 +0530

    Heaps | Pq in jcf

--- Heaps.java
+++ Heaps.java
@@ -1,6 +1,16 @@
+import java.util.PriorityQueue;
 public class Heaps {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.println("(Heaps/PrioriyQueues)");
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        pq.add(4); //add O(N)
+        pq.add(2);
+        pq.add(1);
+        pq.add(3);
+        while(!pq.isEmpty()){
+            System.out.print(pq.remove()+ " ");
+        }
+
     }
     
 }
