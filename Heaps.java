8cde9b67204156c36092a7d1af01159f2cc5c8e5
prince7111 <princemeena7111@gmail.com>
2026-01-18 12:52:43 +0530

    Heaps | 39heaps | pq fo objects | Prince Meena 44(0_1)

--- Heaps.java
+++ Heaps.java
@@ -1,15 +1,32 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 public class Heaps {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name,int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String args[]){
         System.out.println("(Heaps/PrioriyQueues)");
-        PriorityQueue <Integer> pq = new PriorityQueue<>();
-        pq.add(4); //add O(N)
-        pq.add(2);
-        pq.add(1);
-        pq.add(3);
+        PriorityQueue <Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(new Student("A",4));
+        pq.add(new Student("B", 3));
+        pq.add(new Student("C",2));
+        pq.add(new Student("D", 1));
         while(!pq.isEmpty()){
-            System.out.print(pq.remove()+ " ");
+            System.out.println(pq.peek().name+"->"+pq.peek().rank);
+            pq.remove();
         }
+         
+
+
 
     }
     
