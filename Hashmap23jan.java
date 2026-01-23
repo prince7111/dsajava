f64915d725f3385c771f6ceb7e698ff2a0bfe46f
prince7111 <princemeena7111@gmail.com>
2026-01-23 13:36:36 +0530

    Hasing 40hashing | Hashmap Operations

--- Hashmap23jan.java
+++ Hashmap23jan.java
@@ -1,7 +1,19 @@
-import java.util.*;
+import java.util.HashMap;
 public class Hashmap23jan {
     public static void main(String args[]){
         System.out.println("Hello World");
+        HashMap<String,Integer> hsmap = new HashMap<>();
+        //add\put O(n)
+        hsmap.put("A",1);
+        hsmap.put("B",2);
+        hsmap.put("C", 3);
+        System.out.println(hsmap);
+        System.out.println(hsmap.get("A"));
+        System.out.println(hsmap.containsKey("B"));
+         System.out.println(hsmap.containsKey("D"));
+         hsmap.clear();
+         System.out.println("isEmpty:"+hsmap.isEmpty());
+
 
     }    
 }
