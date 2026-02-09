b17ddd0740094166c9ec6a36e56ade071413c95c
prince7111 <princemeena7111@gmail.com>
2026-02-09 11:26:21 +0530

    Added new folder /& futurisitic files

---
+++ Graph/GraphTheory.java
@@ -0,0 +1,9 @@
+package Graph;
+
+public class GraphTheory {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        //Graphs Data Structures
+    }
+    
+}
