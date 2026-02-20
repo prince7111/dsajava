46c3772cec0b30bb0f82ec410f456b7428cf5f8a
prince7111 <princemeena7111@gmail.com>
2026-02-20 21:23:11 +0530

    hasPath compleate

--- Graph/g1p.java
+++ Graph/g1p.java
@@ -1,4 +1,5 @@
 package Graph;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 public class g1p {
@@ -14,17 +15,19 @@ public class g1p {
         for(int i =0; i<graph.length ; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0,2));
+        graph[0].add(new Edge(0,1));
 
+        graph[1].add(new Edge(1,0));
         graph[1].add(new Edge(1,2));
-        graph[1].add(new Edge(1,3));
 
-        graph[2].add(new Edge(2,0));
         graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
 
-        graph[3].add(new Edge(3,1));
-        graph[3].add(new Edge(3,2));
+        graph[3].add(new Edge(3,4));
+        graph[4].add(new Edge(4,3));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,4));
+        
     
     }
     public static void bfs(ArrayList<Edge>graph[],int V,boolean vis[],int st){
@@ -55,20 +58,34 @@ public class g1p {
             }
         }
     }
+    public static boolean hasPath(ArrayList<Edge> graph[],boolean vis[],int s,int d, int c){
+        if(c == d){
+            return true;
+        }
+        vis[c] = true;
+        for(int i=0;i<graph[c].size();i++){
+            Edge e = graph[c].get(i);
+            if(vis[e.dest] == false){
+            boolean rastamila = hasPath(graph, vis, s, d, e.dest);
+            if(rastamila == true){
+                return true;
+            }
+            }
+            
+        }
+        return false;
+    }
     public static void main(String args[]){
-        int V = 4;
-
+        int V = 6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
         boolean vis[] = new boolean[V];
+        int s = 2;
+        int d = 1;
 
-       for(int i =0; i<V;i++){
-        if(vis[i]==false){
-        dfs(graph, vis, i);
-        }
-       }
-       System.out.println();
-   
+       
+        System.out.println( hasPath(graph,vis, s, d ,0));
+
 
 
     }
