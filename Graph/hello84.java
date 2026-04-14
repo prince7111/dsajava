71d0aad153b5c0e3054c7e5a80028fd1c8cccad0
prince7111 <princemeena7111@gmail.com>
2026-04-14 11:21:07 +0530

    create Graph Adj list (Medium)

--- Graph/hello84.java
+++ Graph/hello84.java
@@ -12,21 +12,25 @@ public class hello84{
     }
 
     public static void main(String args[]){
-        int v = 3;
+        int v = 5;
         ArrayList<Edge> graph[] = new ArrayList[v];
           for(int i = 0; i<v; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0,1,1));
-        graph[0].add(new Edge(0,2,1));
+        graph[0].add(new Edge(0,1,0));
+        graph[0].add(new Edge(0,2,0));
+        graph[0].add(new Edge(0,3,0));
+        graph[0].add(new Edge(0,4,0));
 
-        graph[1].add(new Edge(1,0,1));
-        graph[1].add(new Edge(1,2,1));
+        graph[1].add(new Edge(1,0,0));
 
-        graph[2].add(new Edge(2,0,1));
-        graph[2].add(new Edge(2,1,1));
+        graph[2].add(new Edge(2,0,0));
+
+        graph[3].add(new Edge(3,0,0));
+
+        graph[4].add(new Edge(4,0,0));
       
-        for(int i = 0; i<v; i++){
+        for(int i =0; i<v; i++){
             System.out.print("Vertex "+i+ " Neighbour :");
             for(int j = 0;j<graph[i].size();j++){
                 Edge e = graph[i].get(j);
