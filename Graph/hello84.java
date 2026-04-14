6b4680c7583fa86605ac578e775cc5c56dcd8cd7
prince7111 <princemeena7111@gmail.com>
2026-04-14 11:13:57 +0530

    create Graph (Easy)

--- Graph/hello84.java
+++ Graph/hello84.java
@@ -1,42 +1,38 @@
-import java.util.ArrayList;
-
-public class hello84 {
+import java.util.*;
+public class hello84{
     static class Edge{
         int src;
         int dest;
-        Edge(int s,int d){
+        int wt;
+         Edge(int s,int d, int wt){
             this.src = s;
             this.dest = d;
+            this.wt = wt;
         }
-
     }
+
     public static void main(String args[]){
-        System.out.println("Hello world");
-        int v = 5;
+        int v = 3;
         ArrayList<Edge> graph[] = new ArrayList[v];
-        for(int i =0;i<v; i++){
+          for(int i = 0; i<v; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0,1));
-        graph[0].add(new Edge(0,4));
+        graph[0].add(new Edge(0,1,1));
+        graph[0].add(new Edge(0,2,1));
 
-        graph[1].add(new Edge(1,0));
-        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,0,1));
+        graph[1].add(new Edge(1,2,1));
 
-        graph[2].add(new Edge(2,1));
-        graph[2].add(new Edge(2,3));
-
-        graph[3].add(new Edge(3,2));
-        graph[3].add(new Edge(3,4));
-
-        graph[4].add(new Edge(4,0));
-        graph[4].add(new Edge(4,3));
-
-        for(int i=0;i<graph[1].size();i++){
-            Edge e = graph[1].get(i);
-            System.out.println("src "+e.src + " dest "+e.dest);
+        graph[2].add(new Edge(2,0,1));
+        graph[2].add(new Edge(2,1,1));
+      
+        for(int i = 0; i<v; i++){
+            System.out.print("Vertex "+i+ " Neighbour :");
+            for(int j = 0;j<graph[i].size();j++){
+                Edge e = graph[i].get(j);
+                System.out.print(e.dest+" ");  
+            }
+            System.out.println("\n");
         }
-        
     }
-    
 }
