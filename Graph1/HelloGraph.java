3f8bca79ec6117b3bafc94325476e35bc198c861
prince7111 <princemeena7111@gmail.com>
2026-06-11 14:35:49 +0530

    42.6

--- Graph1/HelloGraph.java
+++ Graph1/HelloGraph.java
@@ -8,38 +8,52 @@ public class HelloGraph{
             this.dest = d;
         }
     }
+    public static  void Bfs( ArrayList<Edge> graph[],int V){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] =new boolean[V+1];
+        q.add(1);
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.println(curr);
+                vis[curr] = true;
+                for(int i = 0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                        q.add(e.dest);
+                    
+                }
+
+            }
+        }
+
+    }
     public static void main(String args[]){
         int V = 6;
-        ArrayList<Edge> graph[] = new ArrayList[V];
-        for(int i =0 ; i< V; i++){
+        ArrayList<Edge> graph[] = new ArrayList[V+1];
+        for(int i =0 ; i< V+1; i++){
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(1, 2));
-        graph[0].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
 
-        graph[1].add(new Edge(2, 1));
-        graph[1].add(new Edge(2, 4));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 4));
 
-        graph[2].add(new Edge(3, 1));
-        graph[2].add(new Edge(3, 5));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 5));
 
-        graph[3].add(new Edge(4, 2));
-        graph[3].add(new Edge(4, 5));
-        graph[3].add(new Edge(4, 6));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 5));
+        graph[4].add(new Edge(4, 6));
 
-        graph[4].add(new Edge(5,3));
-        graph[4].add(new Edge(5,4));  
-        graph[4].add(new Edge(5,6));
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));  
+        graph[5].add(new Edge(5,6));
 
-        graph[5].add(new Edge(6,1));
-        graph[5].add(new Edge(6,1));
-        System.out.print(5+" neighbour =");
-        for(int i=0; i<graph[4].size(); i++){
-            Edge e = graph[4].get(i);
-            
-            System.out.print(e.dest+",");
-
-        }
+        graph[6].add(new Edge(6,1));
+        graph[6].add(new Edge(6,1));
+        Bfs(graph,V);
+        
 
     
     }
